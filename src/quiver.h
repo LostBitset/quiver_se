8cd3cf15caf02881fcd25d819def1331391b0a80
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-10 20:24:57 -0500

    Start working on quiver stuff

---
+++ src/quiver.h
@@ -0,0 +1,97 @@
+// <quiver> -*- C++ -*-
+
+/*!
+    A doubly-linked quiver, abstracted over the node, edge, and edge container types. Edge
+    containers must be some form of reversible associative data structure, as they are placed inside
+    each node and map edges to the nodes they lead to. 
+*/
+
+#pragma once
+#include <concepts>
+#include <cstddef>
+#include <map>
+#include <sys/types.h>
+#include <utility>
+#include <vector>
+
+/*! A concept representing a reversible (think doubly-linked) associative data structure. */
+template <typename T, typename K, typename V>
+concept ReversibleAssoc = requires(T item, K key, V value) {
+    { item.insert(key, value) };
+    { item.fwd_lookup(key) } -> std::convertible_to<V*>;
+    { item.rev_lookup(value) } -> std::convertible_to<std::vector<K>>;
+};
+
+// begin forward declarations
+
+struct QuiverNodeRef;
+
+template <typename E, typename R = QuiverNodeRef>
+class SimpleQuiverEdge;
+
+template <typename N, typename E, typename C = SimpleQuiverEdge<E>>
+requires ReversibleAssoc<C, E, QuiverNodeRef>
+class Quiver;
+
+template <typename N, typename E, typename C>
+requires ReversibleAssoc<C, E, QuiverNodeRef>
+struct QuiverNode;
+
+// end forward declarations
+
+/*! A simple set of quiver edges from a single node. Reverse lookups are slow. */
+template <typename E, typename R>
+class SimpleQuiverEdge {
+
+    public:
+    explicit SimpleQuiverEdge();
+    SimpleQuiverEdge(SimpleQuiverEdge<E, R>& other) = default;
+    SimpleQuiverEdge(SimpleQuiverEdge<E, R>&& other) = default;
+
+    // begin ReversibleAssoc methods
+    void insert(E edge, R node_ref);
+    R* fwd_lookup(E edge);
+    std::vector<E> rev_lookup(R node_ref);
+    // end ReversibleAssoc methods
+
+    private:
+    std::map<E, R> backing_map;
+
+};
+
+/*! A doubly-linked quiver, abstracted over the node, edge, and edge container types. */
+template <typename N, typename E, typename C>
+requires ReversibleAssoc<C, E, QuiverNodeRef>
+class Quiver {
+    
+    public:
+    explicit Quiver();
+    Quiver(Quiver<N, E, C>& other) = default;
+    Quiver(Quiver<N, E, C>&& other) = default;
+
+    // *TODO* Methods go here
+
+    private:
+    std::vector<QuiverNode<N, E, C>> arena;
+
+};
+
+/*! A reference to a node inside a quiver. This is just an index, and you need the Quiver object. */
+struct QuiverNodeRef {
+
+    size_t index;
+
+    template <typename N, typename E, typename C>
+    requires ReversibleAssoc<C, E, QuiverNodeRef>
+    QuiverNode<N, E, C>* find_in_quiver(Quiver<N, E, C>* quiver);
+
+};
+
+/*! A node inside a quiver. Not usable without the Quiver object. */
+template <typename N, typename E, typename C>
+requires ReversibleAssoc<C, E, QuiverNodeRef>
+struct QuiverNode {
+
+    // *TODO* Everything
+
+};
