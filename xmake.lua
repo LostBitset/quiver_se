bf40b2ade37ca3fccfedfadc0250698e185c9dd8
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-12 20:21:34 -0500

    Alright xmake seems to work

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,10 @@
 target("libcode")
     set_kind("static")
     add_files("src/lib/*.cpp")
+    set_languages("cxx20")
 
 target("bincode")
     set_kind("binary")
     add_files("src/bin/*.cpp")
     add_deps("libcode")
+    set_languages("cxx20")
