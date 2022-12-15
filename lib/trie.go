a4a82839b31a4e1c72e27e119372d8f22a2c40ab
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-15 18:03:27 -0500

    Add `Lookup` method for tries

--- lib/trie.go
+++ lib/trie.go
@@ -1,16 +1,16 @@
 package qse
 
 // A trie that maps sequences of N to values of L
-type Trie[N any, L comparable] struct {
-	root   TrieNode[N, L]
+type Trie[N comparable, L comparable] struct {
+	root   TrieValueNode[N, L]
 	leaves map[L]struct{}
 }
 
-type TrieNode[N any, L comparable] interface {
+type TrieNode[N comparable, L comparable] interface {
 	IsTrieLeaf() (is bool)
 }
 
-type TrieValueNode[N any, L comparable] struct {
+type TrieValueNode[N comparable, L comparable] struct {
 	value    []N
 	parents  []TrieValueNode[N, L]
 	children []TrieNode[N, L]
--- lib/trie_impl.go
+++ lib/trie_impl.go
@@ -10,7 +10,7 @@ func (TrieLeafNode[L]) IsTrieLeaf() (is bool) {
 	return
 }
 
-func NewTrie[N any, L comparable]() (t Trie[N, L]) {
+func NewTrie[N comparable, L comparable]() (t Trie[N, L]) {
 	t = Trie[N, L]{
 		NewTrieValueNode[N, L](),
 		make(map[L]struct{}),
@@ -18,11 +18,35 @@ func NewTrie[N any, L comparable]() (t Trie[N, L]) {
 	return
 }
 
-func NewTrieValueNode[N any, L comparable]() (node TrieValueNode[N, L]) {
+func NewTrieValueNode[N comparable, L comparable]() (node TrieValueNode[N, L]) {
 	node = TrieValueNode[N, L]{
 		make([]N, 0),
 		make([]TrieValueNode[N, L], 0),
 		make([]TrieNode[N, L], 0),
 	}
 	return
+}
+
+func (t Trie[N, L]) Lookup(seq []N) (ptr *L) {
+	node := t.root
+	cursor := 0
+	for {
+		for _, child := range node.children {
+			if child.IsTrieLeaf() {
+				leaf := child.(TrieLeafNode[L])
+				if cursor == len(seq)-1 {
+					ptr = &leaf.value
+					return
+				}
+			} else {
+				value_node := child.(TrieValueNode[N, L])
+				for _, expected := range value_node.value {
+					if cursor >= len(seq) || seq[cursor] != expected {
+						break
+					}
+					cursor++
+				}
+			}
+		}
+	}
 }
