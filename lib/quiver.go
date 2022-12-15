b10f864cd27498e8aa38a31e0e1f45025d4a9a32
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-15 08:15:20 -0500

    Need aliases too

--- lib/quiver.go
+++ lib/quiver.go
@@ -1,9 +1,14 @@
 package qse
 
 type ReversibleAssoc[A any, B any] interface {
+	Insert(a A, b B)
+	FwdLookup(a A) (item B)
+	RevLookup(b B) (items []A)
 }
 
 type QuiverIndex uint
+
+type SimpleQuiverNode[N any, E any] QuiverNode[N, E, SimpleReversibleAssoc[E, QuiverIndex]]
 
 type QuiverNode[N any, E any, C ReversibleAssoc[E, QuiverIndex]] struct {
 	value   N
