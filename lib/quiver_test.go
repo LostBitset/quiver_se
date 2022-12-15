c1653790a1b3b8eace54c317711a2e5e4656ba02
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-15 18:20:31 -0500

    Fix test

--- lib/quiver_test.go
+++ lib/quiver_test.go
@@ -1,7 +1,6 @@
 package qse
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,7 +11,6 @@ func TestQuiver(t *testing.T) {
 	n1 := q.insert_node_simple(7)
 	n2 := q.insert_node_simple(8)
 	n3 := q.insert_node_simple(9)
-	fmt.Printf("q.arena: %v\n", q.arena)
 	q.insert_edge(n1, n2, 90)
 	q.insert_edge(n1, n2, 60)
 	q.insert_edge(n2, n1, 30)
