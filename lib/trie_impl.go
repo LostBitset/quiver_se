85f505ce972367ee77844c40d15b8abbdc388234
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-16 09:10:24 -0500

    Work on `PrepChild` method

--- lib/trie_impl.go
+++ lib/trie_impl.go
@@ -41,3 +41,25 @@ func (node *TrieValueNode[N, L]) CutPrefix(shared map[N]struct{}) (parent *TrieV
 	node.parents = append(node.parents, parent)
 	return
 }
+
+func (node *TrieValueNode[N, L]) PrepChild(seq map[N]struct{}) (child *TrieNode[N, L]) {
+	var closest *TrieNode[N, L]
+	var closest_by *int
+	for _, child := range node.children {
+		if child.IsTrieLeaf() {
+			continue
+		}
+		child := child.(TrieValueNode[N, L])
+		matches := 0
+		for key := range child.value {
+			if _, ok := seq[key]; ok {
+				matches++
+			}
+		}
+		if matches > *closest_by {
+			// TODO
+		}
+		// TODO
+	}
+	// TODO
+}
