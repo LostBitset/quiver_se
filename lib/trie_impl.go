36bcfbd46a587df55c6c77c7ef2de7746ab84534
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-16 12:23:00 -0500

    Seems to actually sort of work now

--- lib/trie_impl.go
+++ lib/trie_impl.go
@@ -1,5 +1,7 @@
 package qse
 
+import "fmt"
+
 func (TrieValueNode[N, L]) IsTrieLeaf() (is bool) {
 	is = false
 	return
@@ -27,6 +29,24 @@ func NewTrieValueNode[N comparable, L comparable]() (node TrieValueNode[N, L]) {
 	return
 }
 
+func (t Trie[N, L]) String() (repr string) {
+	repr = fmt.Sprintf("Trie{Leaves = %v, Root = [@Root]%v}", t.leaves, t.root)
+	return
+}
+
+func (node TrieValueNode[N, L]) String() (repr string) {
+	repr = fmt.Sprintf(
+		"V[%v]{..., Children = %v}",
+		node.value, node.children,
+	)
+	return
+}
+
+func (node TrieLeafNode[L]) String() (repr string) {
+	repr = fmt.Sprintf("L(%v)", node.value)
+	return
+}
+
 func (node *TrieValueNode[N, L]) CutPrefix(shared map[N]struct{}) (parent *TrieValueNode[N, L]) {
 	parent = &TrieValueNode[N, L]{
 		shared,
@@ -42,10 +62,11 @@ func (node *TrieValueNode[N, L]) CutPrefix(shared map[N]struct{}) (parent *TrieV
 	return
 }
 
-func (node *TrieValueNode[N, L]) PrepChild(seq map[N]struct{}, leaf L) (child TrieNode[N, L]) {
+func (node *TrieValueNode[N, L]) PrepChild(seq map[N]struct{}, leaf L) (r_child TrieNode[N, L]) {
 	var closest TrieNode[N, L]
 	var closest_shared *map[N]struct{}
 	var closest_index *int
+	exact_match := false
 	for index, child := range node.children {
 		if child.IsTrieLeaf() {
 			continue
@@ -62,29 +83,75 @@ func (node *TrieValueNode[N, L]) PrepChild(seq map[N]struct{}, leaf L) (child Tr
 			closest = &child
 			closest_shared = &shared
 			closest_index = &index
+			exact_match = len(shared) == len(child.value)
 		}
 	}
+	if exact_match {
+		r_child = *closest.(*TrieValueNode[N, L])
+		return
+	}
 	if closest == nil {
-		child = &TrieLeafNode[L]{
-			leaf,
+		if len(seq) == 0 {
+			fmt.Println("new branch, leaf node")
+			r_child = &TrieLeafNode[L]{
+				leaf,
+			}
+		} else {
+			fmt.Println("new branch, value node")
+			seq_copy := make(map[N]struct{})
+			for k := range seq {
+				seq_copy[k] = struct{}{}
+			}
+			r_child = &TrieValueNode[N, L]{
+				seq_copy,
+				[]*TrieValueNode[N, L]{
+					node,
+				},
+				make([]TrieNode[N, L], 0),
+			}
 		}
 	} else {
 		target := closest.(TrieValueNode[N, L])
-		node.children[*closest_index] = target.CutPrefix(*closest_shared)
+		parent_ref := target.CutPrefix(*closest_shared)
+		node.children[*closest_index] = *parent_ref
+		rem_seq := make(map[N]struct{})
+		for item := range seq {
+			if _, ok := (*closest_shared)[item]; !ok {
+				rem_seq[item] = struct{}{}
+			}
+		}
+		r_child = TrieValueNode[N, L]{
+			rem_seq,
+			[]*TrieValueNode[N, L]{
+				parent_ref,
+			},
+			make([]TrieNode[N, L], 0),
+		}
 	}
-	node.children = append(node.children, child)
+	node.children = append(node.children, r_child)
 	return
 }
 
 func (t *Trie[N, L]) Insert(seq map[N]struct{}, leaf L) {
 	node := &t.root
+	seq_copy := make(map[N]struct{})
+	for k := range seq {
+		seq_copy[k] = struct{}{}
+	}
 	for {
-		child := node.PrepChild(seq, leaf)
+		child := node.PrepChild(seq_copy, leaf)
 		if child.IsTrieLeaf() {
 			break
 		} else {
-			child := child.(TrieValueNode[N, L])
-			node = &child
+			switch child := child.(type) {
+			case *TrieValueNode[N, L]:
+				node = child
+			case TrieValueNode[N, L]:
+				node = &child
+			}
+		}
+		for taken := range node.value {
+			delete(seq_copy, taken)
 		}
 	}
 }
--- lib/trie_test.go
+++ lib/trie_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestTrie(t *testing.T) {
 	trie := NewTrie[int, int]()
-	/*trie.Insert([]int{0, 0, 7}, 44)
-	trie.Insert([]int{0, 0, 8}, 55)
-	*/fmt.Printf("trie: %v\n", trie)
+	trie.Insert(map[int]struct{}{0: {}, 1: {}, 7: {}}, 44)
+	//trie.Insert(map[int]struct{}{0: {}, 1: {}, 8: {}}, 55)
+	fmt.Printf("trie: %v\n", trie)
 	/*assert.Equal(
 		t,
 		44,
