3ff44d1fb6e0361b1886f01e1ed416a93f9d07cd
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-16 15:40:00 -0500

    Still working

--- lib/trie_test.go
+++ lib/trie_test.go
@@ -12,11 +12,6 @@ func TestTrie(t *testing.T) {
 	trie.Insert(map[int]struct{}{0: {}, 1: {}, 8: {}}, 55)
 	trie.Insert(map[int]struct{}{9: {}}, 99)
 	trie.Insert(map[int]struct{}{0: {}, 5: {}, 6: {}, 7: {}, 4: {}}, 31)
-	//trie.Insert(map[int]struct{}{0: {}, 5: {}, 6: {}, 8: {}, 4: {}}, 32)
+	trie.Insert(map[int]struct{}{0: {}, 5: {}, 6: {}, 8: {}, 4: {}}, 32)
 	fmt.Printf("trie: %v\n", trie)
-	/*assert.Equal(
-		t,
-		44,
-		trie.Lookup([]int{0, 0, 7}),
-	)*/
 }
