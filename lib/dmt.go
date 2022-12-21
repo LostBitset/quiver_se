70dc1dcfcccbe4a416f6708010d016c0487a0dc2
Landon <102832670+LostBitset@users.noreply.github.com>
2022-12-21 18:07:46 -0500

    Actually never mind, I can do this later

--- lib/dmt.go
+++
@@ -1,9 +0,0 @@
-package qse
-
-// A trie representing mappings from clauses of a disjunctive normal form (DNF), with literals of
-// type LIT, to values of type LEAF. The Merkle component comes from the fact that the hashes of
-// child subtries are kept and recomputed as necessary in order to make it fast to simplify clause
-// pairs of the form (A && B) || (A && !B) to just A.
-type DnfMerkleTrie[LIT comparable, LEAF comparable] struct {
-	backing_trie MerkleTrie[LIT, LEAF]
-}
