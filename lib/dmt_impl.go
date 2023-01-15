e5839108a833ada946ef85933b219cb8df971f00
Landon <102832670+LostBitset@users.noreply.github.com>
2023-01-15 08:29:55 -0500

    just need to add simplification bit

--- lib/dmt_impl.go
+++ lib/dmt_impl.go
@@ -14,6 +14,19 @@ func (lit Literal[NODE]) Hash() (digest []byte) {
 	return
 }
 
+func ZeroDigest() (digest []byte) {
+	digest = make([]byte, 4)
+	return
+}
+
+func FixDigest(pseudo_digest []byte, fix_seed byte) (digest []byte) {
+	hasher := fnv.New32a()
+	hasher.Write(pseudo_digest)
+	hasher.Write([]byte{fix_seed})
+	digest = hasher.Sum([]byte{})
+	return
+}
+
 func BufferingLiteral[NODE hashable](value NODE) (lit Literal[NODE]) {
 	lit = Literal[NODE]{value, true}
 	return
@@ -31,7 +44,7 @@ func NewDMT[NODE hashable, LEAF comparable]() (t DMT[NODE, LEAF]) {
 				map[Literal[NODE]]struct{}{},
 				nil,
 				[]TrieNode[Literal[NODE], LEAF]{},
-				make([]byte, 4),
+				ZeroDigest(),
 			},
 			map[LEAF][]*TrieLeafNode[Literal[NODE], LEAF, []byte]{},
 		},
@@ -50,4 +63,35 @@ func (t *DMT[NODE, LEAF]) Insert(
 }
 
 func (t *DMT[NODE, LEAF]) UpdateHashes(leaf_ptr *TrieLeafNode[Literal[NODE], LEAF, digest_t]) {
+	var node *TrieValueNode[Literal[NODE], LEAF, digest_t]
+	node = leaf_ptr.parent
+correctLoop:
+	for {
+		sum := ZeroDigest()
+	xorChildrenLoop:
+		for _, raw_child := range node.children {
+			var child *TrieValueNode[Literal[NODE], LEAF, digest_t]
+			switch c := raw_child.(type) {
+			case TrieLeafNode[Literal[NODE], LEAF, digest_t]:
+				continue xorChildrenLoop
+			case *TrieLeafNode[Literal[NODE], LEAF, digest_t]:
+				continue xorChildrenLoop
+			case TrieValueNode[Literal[NODE], LEAF, digest_t]:
+				child = &c
+			case *TrieValueNode[Literal[NODE], LEAF, digest_t]:
+				child = c
+			}
+			for i, byte_value := range child.meta {
+				sum[i] ^= byte_value
+			}
+		}
+		*&node.meta = FixDigest(sum, 0x3E) // Update the hash
+		t.SimplifyNode(node) // Try and locally simplify
+		node = node.parent
+		if node == nil {
+			break correctLoop
+		}
+	}
 }
+
+func (t *DMT[NODE, LEAF]) SimplifyNode(node *TrieValueNode[Literal[NODE], LEAF, digest_t]) {}
