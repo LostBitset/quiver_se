33980bc9d44c1ad4e083289f9d75b1c68d648862
Landon <102832670+LostBitset@users.noreply.github.com>
2023-01-16 11:45:37 -0500

    Yeehee all green

--- lib/dmt_test.go
+++ lib/dmt_test.go
@@ -50,6 +50,23 @@ func TestTransformingDMT(t *testing.T) {
 		uint32_H{1},
 	}
 	dmt.Insert(entry.key, entry.value)
-	entries := []TrieEntry[Literal[uint32_H], uint32_H]{}
+	entries := []TrieEntry[Literal[uint32_H], uint32_H]{
+		{
+			map[Literal[uint32_H]]struct{}{
+				{uint32_H{14}, true}:  {},
+				{uint32_H{16}, false}: {},
+				{uint32_H{17}, false}: {},
+			},
+			uint32_H{1},
+		},
+		{
+			map[Literal[uint32_H]]struct{}{
+				{uint32_H{14}, false}: {},
+				{uint32_H{18}, true}:  {},
+				{uint32_H{19}, true}:  {},
+			},
+			uint32_H{2},
+		},
+	}
 	assert.ElementsMatch(t, dmt.EntryList(), entries)
 }
