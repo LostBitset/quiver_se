44c164835c5cdb2fc9dec954f2f337b6ddb8f782
Landon <102832670+LostBitset@users.noreply.github.com>
2023-01-16 09:51:40 -0500

    need to add `EntryList` for dmt

--- lib/dmt_test.go
+++ lib/dmt_test.go
@@ -6,7 +6,39 @@ import (
 	// "github.com/stretchr/testify/assert"
 )
 
-func CreateExampleDMT() (dmt DMT[uint32_H, uint32_H]) {
+func CreateExampleDMT() (dmt DMT[uint32_H, uint32_H], entries []TrieEntry[Literal[uint32_H], uint32_H]) {
+	dmt = NewDMT[uint32_H, uint32_H]()
+	entries = []TrieEntry[Literal[uint32_H], uint32_H]{
+		{
+			map[Literal[uint32_H]]struct{}{
+				{uint32_H{14}, true}: {},
+				{uint32_H{15}, true}: {},
+				{uint32_H{16}, false}: {},
+				{uint32_H{17}, false}: {},
+			},
+			uint32_H{1},
+		},
+		{
+			map[Literal[uint32_H]]struct{}{
+				{uint32_H{14}, true}: {},
+				{uint32_H{15}, false}: {},
+				{uint32_H{16}, false}: {},
+				{uint32_H{17}, false}: {},
+			},
+			uint32_H{1},
+		},
+		{
+			map[Literal[uint32_H]]struct{}{
+				{uint32_H{14}, false}: {},
+				{uint32_H{18}, true}: {},
+				{uint32_H{19}, true}: {},
+			},
+			uint32_H{2},
+		},
+	}
+	for _, entry := range entries {
+		dmt.Insert(entry.key, entry.value)
+	}
 	return
 }
 
