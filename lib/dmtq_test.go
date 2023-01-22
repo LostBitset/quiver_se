51f27949db9c8190321480ab30fc6c42f6121e6d
Landon <102832670+LostBitset@users.noreply.github.com>
2023-01-22 12:08:00 -0500

    all tests passed :)

--- lib/dmtq_test.go
+++ lib/dmtq_test.go
@@ -8,4 +8,37 @@ import (
 
 func TestDMTQ(t *testing.T) {
 	var q Quiver[uint32_H, map[Literal[uint32_H]]struct{}, *DMT[uint32_H, QuiverIndex]]
+	n1_container := NewDMT[uint32_H, QuiverIndex]()
+	n2_container := NewDMT[uint32_H, QuiverIndex]()
+	n1 := q.insert_node(uint32_H{1}, &n1_container)
+	n2 := q.insert_node(uint32_H{2}, &n2_container)
+	q.insert_edge(
+		n1,
+		n2,
+		map[Literal[uint32_H]]struct{}{
+			{uint32_H{77}, true}: {},
+			{uint32_H{88}, true}: {},
+		},
+	)
+	q.insert_edge(
+		n1,
+		n2,
+		map[Literal[uint32_H]]struct{}{
+			{uint32_H{77}, true}: {},
+			{uint32_H{88}, false}: {},
+		},
+	)
+	assert.ElementsMatch(
+		t,
+		[]Neighbor[map[Literal[uint32_H]]struct{}]{
+			{
+				map[Literal[uint32_H]]struct{}{
+					{uint32_H{77}, true}: {},
+				},
+				n2,
+			},
+		},
+		q.all_outneighbors(n1),
+	)
 }
+
