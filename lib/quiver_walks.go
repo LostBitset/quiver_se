3011cae34c463e1f85f9bad7c9345fab73de8f96
Landon <102832670+LostBitset@users.noreply.github.com>
2023-01-30 15:45:17 -0500

    Just progress - has todos

--- lib/quiver_walks.go
+++ lib/quiver_walks.go
@@ -1,16 +1,38 @@
 package qse
 
+// A nicely packaged way to represent association with a particular quiver
+type PhantomQuiverAssociation[N any, E any, C ReversibleAssoc[E, QuiverIndex]] struct {
+	phantom_node PhantomData[N]
+	phantom_edge PhantomData[E]
+	phantom_cont PhantomData[C]
+}
+
+// An alternative to the QuiverIndex type that carries type parameters
+type QuiverIndexParameterized[N any, E any, C ReversibleAssoc[E, QuiverIndex]] struct {
+	QuiverIndex
+	phantom_association PhantomQuiverAssociation[N, E, C]
+}
+
 type QuiverWalk[N any, E any] struct {
-	start N
+	start QuiverIndex
 	edges []E
 }
 
 // A new edge (and possibly a new node) being added to a quiver
 // The new_dst key indicates whether dst is already a part of the quiver or not
-type QuiverUpdate[N any, E any] struct {
-	src     N
-	dst     N
-	edge    E
-	new_dst bool
+type QuiverUpdate[N any, E any, C ReversibleAssoc[E, QuiverIndex]] struct {
+	src  QuiverIndex
+	dst  QuiverUpdateDst[N, E, C]
+	edge E
 }
 
+// A non-trivial QuiverUpdateDst - a node itself that has to be added to the quiver
+// But this must be wrapped with it's intent (mostly for type parameterization)
+type QuiverIntendedNode[N any, E any, C ReversibleAssoc[E, QuiverIndex]] struct {
+	node                N
+	phantom_association PhantomQuiverAssociation[N, E, C]
+}
+
+type QuiverUpdateDst[N any, E any, C ReversibleAssoc[E, QuiverIndex]] interface {
+	ResolveAsQuiverUpdateDst(q_ptr *Quiver[N, E, C]) (ref QuiverIndex)
+}
---
+++ lib/quiver_walks_impl.go
@@ -0,0 +1,57 @@
+package qse
+
+func NewPhantomQuiverAssociation[N any, E any, C ReversibleAssoc[E, QuiverIndex]]() (
+	phantom_association PhantomQuiverAssociation[N, E, C],
+) {
+	phantom_association = PhantomQuiverAssociation[N, E, C]{
+		PhantomData[N]{},
+		PhantomData[E]{},
+		PhantomData[C]{},
+	}
+	return
+}
+
+func TrustingParameterizeQuiverIndex[N any, E any, C ReversibleAssoc[E, QuiverIndex]](
+	index QuiverIndex,
+) (
+	indexp QuiverIndexParameterized[N, E, C],
+) {
+	indexp = QuiverIndexParameterized[N, E, C]{
+		index,
+		NewPhantomQuiverAssociation[N, E, C](),
+	}
+	return
+}
+
+func (indexp QuiverIndexParameterized[N, E, C]) Unparameterize() (index QuiverIndex) {
+	index = indexp.QuiverIndex
+	return
+}
+
+func (Quiver[N, E, C]) ParameterizeIndex(index QuiverIndex) (indexp QuiverIndexParameterized[N, E, C]) {
+	indexp = TrustingParameterizeQuiverIndex[N, E, C](index)
+	return
+}
+
+func MarkIntentToBeQuiverNode[N any, E any, C ReversibleAssoc[E, QuiverIndex]](node N) (
+	intended_node QuiverIntendedNode[N, E, C],
+) {
+	intended_node = QuiverIntendedNode[N, E, C]{
+		node,
+		NewPhantomQuiverAssociation[N, E, C](),
+	}
+	return
+}
+
+func (Quiver[N, E, C]) MarkNodeIntent(node N) (intended_node QuiverIntendedNode[N, E, C]) {
+	intended_node = MarkIntentToBeQuiverNode[N, E, C](node)
+	return
+}
+
+func (index QuiverIndexParameterized[N, E, C]) ResolveAsQuiverUpdateDst(q_ptr *Quiver[N, E, C]) {
+	// TODO
+}
+
+func (intended_node QuiverIntendedNode[N, E, C]) ResolveAsQuiverUpdateDst(q_ptr *Quiver[N, E, C]) {
+	// TODO
+}
