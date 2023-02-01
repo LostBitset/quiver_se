670622746202985102dfde71c2cd20dcfd906cb0
Landon <102832670+LostBitset@users.noreply.github.com>
2023-01-31 19:03:31 -0500

    Whyyyyyyyyy

--- lib/quiver_walks_test.go
+++ lib/quiver_walks_test.go
@@ -1,12 +1,13 @@
 package qse
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestQuiverUpdatesAcyclic(t *testing.T) {
+func TestQuiverUpdates(t *testing.T) {
 	var q SimpleQuiver[int, int]
 	n1 := q.InsertNodeSimple(7)
 	n2 := q.InsertNodeSimple(8)
@@ -51,7 +52,7 @@ func TestQuiverUpdatesAcyclic(t *testing.T) {
 	)
 }
 
-func TestQuiverUpdatesCyclic(t *testing.T) {
+func TestQuiverUpdatesInduceCycle(t *testing.T) {
 	var q SimpleQuiver[int, int]
 	n1 := q.InsertNodeSimple(7)
 	n2 := q.InsertNodeSimple(8)
@@ -139,6 +140,11 @@ func TestQuiverWalks(t *testing.T) {
 		},
 		q.AllInneighbors(n3),
 	)
+	assert.ElementsMatch(
+		t,
+		[]Neighbor[int]{},
+		q.AllOutneighbors(n3),
+	)
 	walks := make([][]int, 0)
 	for walk_chunked := range walks_chan {
 		new_walk := make([]int, 0)
@@ -154,6 +160,71 @@ func TestQuiverWalks(t *testing.T) {
 			{60, 30, 99},
 			{90, 30, 99},
 		},
+		walks,
+	)
+}
+
+func TestQuiverWalksInduceCycle(t *testing.T) {
+	var q SimpleQuiver[int, int]
+	n1 := q.InsertNodeSimple(7)
+	n2 := q.InsertNodeSimple(8)
+	n3 := q.InsertNodeSimple(9)
+	q.InsertEdge(n1, n2, 90)
+	q.InsertEdge(n1, n2, 60)
+	q.InsertEdge(n2, n1, 30)
+	q.InsertEdge(n2, n3, 44)
+	q.InsertEdge(n1, n3, 55)
+	q.InsertEdge(n1, n3, 77)
+	update := QuiverUpdate[int, int, *SimpleReversibleAssoc[int, QuiverIndex]]{
+		n3,
+		q.ParameterizeIndex(n1),
+		88,
+	}
+	walks_chan := make(chan QuiverWalk[int, int])
+	q.ApplyUpdateAndEmitWalks(
+		walks_chan,
+		update,
+		q.ParameterizeIndex(n1),
+	)
+	assert.ElementsMatch(
+		t,
+		[]Neighbor[int]{
+			{90, n2},
+			{60, n2},
+			{55, n3},
+			{77, n3},
+		},
+		q.AllOutneighbors(n1),
+	)
+	assert.ElementsMatch(
+		t,
+		[]Neighbor[int]{
+			{44, n2},
+			{55, n1},
+			{77, n1},
+		},
+		q.AllInneighbors(n3),
+	)
+	assert.ElementsMatch(
+		t,
+		[]Neighbor[int]{
+			{88, n1},
+		},
+		q.AllOutneighbors(n3),
+	)
+	walks := make([][]int, 0)
+	for walk_chunked := range walks_chan {
+		new_walk := make([]int, 0)
+		for _, chunk := range walk_chunked.edges_chunked {
+			new_walk = append(new_walk, *chunk...)
+			new_walk = append(new_walk, -1) // Boundaries
+		}
+		walks = append(walks, new_walk)
+	}
+	fmt.Println(walks)
+	assert.ElementsMatch(
+		t,
+		[][]int{},
 		walks,
 	)
 }
