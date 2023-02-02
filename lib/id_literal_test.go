49773cd0fc84b6ae0f8d26a2a80432e4d7284384
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-02 11:08:49 -0500

    Alright now its time for the string solver thing

--- lib/id_literal_test.go
+++ lib/id_literal_test.go
@@ -2,8 +2,21 @@ package qse
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIdLiteral(t *testing.T) {
-	// TODO
+	var idsrc IdSource
+	var lit1 IdLiteral[uint]
+	var lit2 IdLiteral[uint]
+	lit1 = IdLiteral[uint]{
+		WithId_H[uint]{42, idsrc.Gen()},
+		false,
+	}
+	lit2 = IdLiteral[uint]{
+		WithId_H[uint]{42, idsrc.Gen()},
+		false,
+	}
+	assert.True(t, lit1.value.Hash32() != lit2.value.Hash32())
 }
