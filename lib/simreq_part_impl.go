3664984397fa4ef774a91c3111a5d876c4863ad9
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-02 21:58:34 -0500

    Need dmtq warden to keep track of `SMTFreeFun`s

--- lib/simreq_part_impl.go
+++ lib/simreq_part_impl.go
@@ -15,12 +15,50 @@ func StartSiMReQ[
 		SCTX,
 	],
 ](
-	in_updates chan QuiverUpdate[
-		QNODE, PHashMap[Literal[WithId_H[ATOM]], struct{}], *DMT[WithId_H[ATOM], QuiverIndex],
-	],
+	in_updates chan DSEWithCallbacksUpdate[QNODE, ATOM, IDENT, SORT],
 	out_models chan MODEL,
 	sys SYS,
 	idsrc IdSource,
 ) {
-	// TODO
+	quiver_updates := make(chan QuiverUpdate[
+		QNODE, PHashMap[Literal[WithId_H[ATOM]], struct{}], *DMT[WithId_H[ATOM], QuiverIndex],
+	])
+	walks := make(chan WithSMTFreeFuns[
+		QuiverWalk[QNODE, PHashMap[Literal[WithId_H[ATOM]], struct{}]],
+		IDENT, SORT,
+	])
+	canidates := make(chan SMTQueryDNFClause[ATOM, IDENT, SORT])
+	smr_config := NewSMRConfig[ATOM, IDENT, SORT, MODEL, SCTX, SYS](
+		canidates, out_models, sys,
+	)
+	var dmtq Quiver[QNODE, PHashMap[Literal[WithId_H[ATOM]], struct{}], *DMT[WithId_H[ATOM], QuiverIndex]]
+	top_node_dmt := NewDMT[WithId_H[ATOM], QuiverIndex]()
+	var zero_node QNODE
+	top_node := dmtq.InsertNode(zero_node, &top_node_dmt)
+	warden_config := DMTQWardenConfig[QNODE, WithId_H[ATOM]]{
+		in_updates: quiver_updates,
+		out_walks:  walks,
+		walk_src:   top_node,
+		dmtq:       dmtq,
+	}
+	smr_config.Start()
+	go func() {
+		defer close(canidates)
+		for walk_recv := range walks {
+			walk_chunked := walk_recv.value
+			walk := make([]IdLiteral[ATOM], 0)
+			for _, chunk := range walk_chunked.edges_chunked {
+				for _, set := range *chunk {
+					stdlib_set := set.ToStdlibMap()
+					for key := range stdlib_set {
+						walk = append(walk, IdLiteral[ATOM](key))
+					}
+				}
+			}
+			canidates <- SMTQueryDNFClause[ATOM, IDENT, SORT]{
+				walk,
+				walk_recv.free_funs,
+			}
+		}
+	}()
 }
