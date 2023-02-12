25a03ee56bd3992fa6c32eb4ee1ad1db69b2be4a
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-12 11:30:20 -0500

    basic jalangi2 test works now

---
+++ js_concolic/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "[js_concolic-test] Testing Jalangi2 (not the actual DSE engine)..."
+
+EXAMPLE_JALANGI2_OUTPUT=$(mktemp)
+
+./jalangi2_analyse.sh --analysis test_analysis.js test_analysis_prgm.js 2>&1 >$EXAMPLE_JALANGI2_OUTPUT
+
+if diff test_analysis_output.txt $EXAMPLE_JALANGI2_OUTPUT; then
+	echo "[js_concolic-test] [ok] Jalangi2 appears to work properly (example analysis passed)."
+else
+	echo "[js_concolic-test] [!!] Jalangi2 example analysis FAILED!"
+	exit 1
+fi
+
+echo "[js_concolic-test] Testing (of) Jalangi2 completed."
+
