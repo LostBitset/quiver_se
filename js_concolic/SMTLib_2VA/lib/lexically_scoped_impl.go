718f487f2ee1f081025007cc04134ab86c383fb9
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-13 20:07:16 -0500

    Update, store more than one indices

--- js_concolic/SMTLib_2VA/lib/lexically_scoped_impl.go
+++ js_concolic/SMTLib_2VA/lib/lexically_scoped_impl.go
@@ -5,7 +5,18 @@ package smtlib2va
 func NewLexicallyScoped() (lvbls LexicallyScoped) {
 	lvbls = LexicallyScoped{
 		NewSliceStack[[]Var](),
-		make(map[string]LexicallyScopedIndex),
+		make(map[string][]LexicallyScopedIndex),
 	}
 	return
+}
+
+func (lvbls LexicallyScoped) EnterScope() {
+	lvbls.stack.Push(make([]Var, 0))
+}
+
+func (lvbls LexicallyScoped) LeaveScope() {
+	vars := lvbls.stack.Peek()
+	for _, v := range vars {
+		delete(lvbls.names, v.name)
+	}
 }
