9476765efb6d5167849faf23b0a36eaee720835d
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-14 16:47:50 -0500

    Great, onto actual transpilation logic

--- js_concolic/SMTLib_2VA/bin/example.TRANSPILED-orig_smt2va.smt
+++ js_concolic/SMTLib_2VA/bin/example.TRANSPILED-orig_smt2va.smt
@@ -1,10 +1,11 @@
 (set-option :produce-unsat-cores true)
-(set-option :smt.core.minimize true) ;; *z3 specific*
+(set-option :smt.core.minimize true) 
 
 (*/enter-scope/*)
 (*/decl-var/* **x)
 (*/enter-scope/*)
 (*/enter-scope/*)
+(*/write-var/* **x "just \"s and (*/enter-scope/*)")
 (*/write-var/* **x true)
 (*/leave-scope/*)
 (assert (*/read-var/* **x))
--- js_concolic/SMTLib_2VA/lib/transpilation_impl.go
+++ js_concolic/SMTLib_2VA/lib/transpilation_impl.go
@@ -9,11 +9,12 @@ import (
 // lvbls = lo vanbi be lo snicne = the environment of the variables
 
 func TranspileV2From2VA(src_2va string) (src_v2 string) {
-	sb := strings.Builder{}
+	comments_re := regexp.MustCompile(`;.*`)
+	wo_comments := comments_re.ReplaceAllString(src_2va, "")
 	strs_re := regexp.MustCompile(`\"(?:[^\\\"]|\\.)*\"`)
 	string_lits := make([]string, 0)
 	wo_strings := strs_re.ReplaceAllStringFunc(
-		src_2va,
+		wo_comments,
 		func(orig string) (repl string) {
 			repl = "<<tmp:string>>@" + strconv.Itoa(len(string_lits))
 			string_lits = append(string_lits, orig)
