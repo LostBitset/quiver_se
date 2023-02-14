20c21bb7f2c0d5794f3bf08618e5263b159a7b22
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-14 16:37:50 -0500

    Now don't have to bother with strings :)

--- js_concolic/SMTLib_2VA/lib/transpilation_impl.go
+++ js_concolic/SMTLib_2VA/lib/transpilation_impl.go
@@ -13,7 +13,8 @@ func TranspileV2From2VA(src_2va string) (src_v2 string) {
 	strs_re := regexp.MustCompile(`\"(?:[^\\\"]|\\.)*\"`)
 	string_lits := make([]string, 0)
 	wo_strings := strs_re.ReplaceAllStringFunc(
-		src_2va, func(orig string) (repl string) {
+		src_2va,
+		func(orig string) (repl string) {
 			repl = "<<tmp:string>>@" + strconv.Itoa(len(string_lits))
 			string_lits = append(string_lits, orig)
 			return
@@ -21,6 +22,14 @@ func TranspileV2From2VA(src_2va string) (src_v2 string) {
 	)
 	wo_strings_transpiled := TranspileV2From2VANoStrings(wo_strings)
 	cut_strs_re := regexp.MustCompile(`<<tmp\:string>>@\d+`)
-	src_v2 = cut_strs_re.ReplaceAllStringFunc()
+	src_v2 = cut_strs_re.ReplaceAllStringFunc(
+		wo_strings_transpiled,
+		func(orig string) (repl string) {
+			_, index_str, _ := strings.Cut(orig, "@")
+			index, _ := strconv.Atoi(index_str)
+			repl = string_lits[index]
+			return
+		},
+	)
 	return
 }
