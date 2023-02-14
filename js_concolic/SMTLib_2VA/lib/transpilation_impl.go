3d21c0571920af5cf8bcbeefa0ce6e412500b78c
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-14 15:29:27 -0500

    Start finishing up this bit

--- js_concolic/SMTLib_2VA/lib/transpilation_impl.go
+++ js_concolic/SMTLib_2VA/lib/transpilation_impl.go
@@ -1,6 +1,24 @@
 package smtlib2va
 
+import "strings"
+
+// lvbls = lo vanbi be lo snicne = the environment of the variables
+
 func TranspileV2From2VA(src_2va []byte) (src_v2 string) {
-	src_v2 = string(src_2va)
+	sb := strings.Builder{}
+	lvbls := NewLexicallyScoped()
+	RewriteSexps(src_2va, sb, map[string](func() string){
+		"*/enter-scope/*": func() (rewritten string) {
+			lvbls.EnterScope()
+			rewritten = ""
+			return
+		},
+		"*/leave-scope/*": func() (rewritten string) {
+			lvbls.LeaveScope()
+			rewritten = ""
+			return
+		},
+	})
+	src_v2 = sb.String()
 	return
 }
