e64f9e4cb6a16099f312dfa7e5d6f2e68ab07132
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-14 16:03:05 -0500

    Better idea

--- js_concolic/SMTLib_2VA/lib/rewrite_sexprs_impl.go
+++
@@ -1,9 +0,0 @@
-package smtlib2va
-
-import (
-	"strings"
-)
-
-func RewriteSexprs(orig []byte, out strings.Builder, rewrites map[string](func() string)) {
-	// TODO
-}
--- js_concolic/SMTLib_2VA/lib/transpilation_impl.go
+++ js_concolic/SMTLib_2VA/lib/transpilation_impl.go
@@ -9,19 +9,6 @@ import (
 func TranspileV2From2VA(src_2va []byte) (src_v2 string) {
 	sb := strings.Builder{}
 	lvbls := NewLexicallyScoped()
-	RewriteSexprs(src_2va, sb, map[string](func() string){
-		"*/enter-scope/*": func() (rewritten string) {
-			lvbls.EnterScope()
-			rewritten = ""
-			return
-		},
-		"*/leave-scope/*": func() (rewritten string) {
-			lvbls.LeaveScope()
-			rewritten = ""
-			return
-		},
-		// TODO (more)
-	})
 	src_v2 = sb.String()
 	return
 }
