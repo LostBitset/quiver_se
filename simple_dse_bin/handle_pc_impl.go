d359a168b4629aea0d6e213107bb435f87eb4935
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-17 15:04:45 -0500

    Ready to generate alternates

--- simple_dse_bin/handle_pc_impl.go
+++ simple_dse_bin/handle_pc_impl.go
@@ -8,7 +8,17 @@ import (
 )
 
 func PathConditionToAnalyzeMessages(msg eidin.PathCondition) (msgs [][]byte) {
-	free_funs := make([]qse.SMTFreeFun[string, string], 0)
+	pc, free_funs := PathConditionMessageToConjunction(msg)
+	var idsrc qse.IdSource
+	sys := qse.SMTLibv2StringSystem{Idsrc: idsrc}
+	return
+}
+
+func PathConditionMessageToConjunction(msg eidin.PathCondition) (
+	conjunction []qse.IdLiteral[string],
+	free_funs []qse.SMTFreeFun[string, string],
+) {
+	free_funs = make([]qse.SMTFreeFun[string, string], 0)
 	for _, free_fun_ref := range msg.GetFreeFuns() {
 		free_fun := *free_fun_ref
 		free_funs = append(
@@ -20,7 +30,7 @@ func PathConditionToAnalyzeMessages(msg eidin.PathCondition) (msgs [][]byte) {
 			},
 		)
 	}
-	pc := make([]qse.IdLiteral[string], 0)
+	conjunction = make([]qse.IdLiteral[string], 0)
 	for _, segment_ref := range msg.GetSegmentedPc() {
 		segment := *segment_ref
 		for _, constraint_ref := range segment.GetPartialPc() {
@@ -28,8 +38,8 @@ func PathConditionToAnalyzeMessages(msg eidin.PathCondition) (msgs [][]byte) {
 			hasher := fnv.New32a()
 			hasher.Write([]byte(constraint.GetConstraint()))
 			hash := hasher.Sum32()
-			pc = append(
-				pc,
+			conjunction = append(
+				conjunction,
 				qse.IdLiteral[string](
 					qse.Literal[qse.WithId_H[string]]{
 						Value: qse.WithId_H[string]{
