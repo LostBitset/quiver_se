505d4bfab3bc447260d91015610babce3a8e668e
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-19 10:52:37 -0500

    Add plan comment

--- QUIP/lib/quip_process.go
+++ QUIP/lib/quip_process.go
@@ -4,6 +4,12 @@ import (
 	q "LostBitset/quiver_se/lib"
 )
 
+// QUIP does the following:
+// - Runs simple DSE
+// - Runs partial DSE on new callbacks
+// - Listens for path conditions, updating the quiver accordingly
+// - Sends Analyze requests in response to SiMReQ
+
 func StartQUIP(
 	out_updates chan q.Augmented[
 		q.QuiverUpdate[
