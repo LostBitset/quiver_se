9b256fbf7b74cbf95e5ad50ce9bd028157fb294d
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-19 20:38:43 -0500

    A new TODO

--- QUIP/lib/perform_dse.go
+++ QUIP/lib/perform_dse.go
@@ -5,6 +5,7 @@ import (
 )
 
 const SUBROUTINE_DSE_TIMEOUT_MILLIS = 10000
+const SUBROUTINE_DSE_CYCLE_WAIT_TIME_MILLIS = 100
 
 // Should generally be called as a goroutine
 func PerformDse(
@@ -13,5 +14,11 @@ func PerformDse(
 	single_callback_mode bool,
 	pc_chan chan eidin.PathCondition,
 ) {
+	go RunSimpleDSE(msg_prefix, SUBROUTINE_DSE_CYCLE_WAIT_TIME_MILLIS)
+	go RunAnalyzer(location, msg_prefix)
+	UsePathConditionChannel(msg_prefix, pc_chan)
+}
+
+func UsePathConditionChannel(msg_prefix string, pc_chan chan eidin.PathCondition) {
 	// TODO
 }
