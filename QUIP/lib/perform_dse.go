c93f2f79284b62cac8098afe88ff07bd7dd2ad60
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-19 20:17:30 -0500

    Add an important constant

--- QUIP/lib/perform_dse.go
+++ QUIP/lib/perform_dse.go
@@ -4,6 +4,8 @@ import (
 	eidin "LostBitset/quiver_se/EIDIN/proto_lib"
 )
 
+const SUBROUTINE_DSE_TIMEOUT_MILLIS = 10000
+
 // Should generally be called as a goroutine
 func PerformDse(location string, msg_prefix string, pc_chan chan eidin.PathCondition) {
 	// TODO
