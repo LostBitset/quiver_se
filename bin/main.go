0eaeb4ef5b2fa22ae384f4c36ad60e9566dcceda
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-21 16:53:03 -0500

    More work more work

--- bin/main.go
+++ bin/main.go
@@ -12,13 +12,22 @@ func SanityCheck() {
 	fmt.Println(greeting)
 }
 
+// Everything enclosed between "// bgn EXAMPLE SPECIFIC" and
+// "// end EXAMPLE SPECIFIC" is, as you might have guessed,
+// specific to the given example. This is all information that
+// can easily be generated automatically, I just don't have the
+// time to implement that right now. This is a research project,
+// and I just want to see if the idea holds any merit right now.
+
 func main() {
 	SanityCheck()
 	cwd, err_cwd := os.Getwd()
 	if err_cwd != nil {
 		panic(err_cwd)
 	}
+	// bgn EXAMPLE SPECIFIC
 	target := cwd + "/example._fninf.js"
+	// end EXAMPLE SPECIFIC
 	msg_prefix := GetMessagePrefix(target)
 	in_updates := make(chan qse.Augmented[
 		qse.QuiverUpdate[
@@ -41,6 +50,43 @@ func main() {
 	](
 		in_updates, out_models, sys,
 	)
+	known_callbacks := make(map[int]qse.QuiverIndex)
+	// bgn EXAMPLE SPECIFIC
+	known_callback_locations := []int{170, 355, 489}
+	// end EXAMPLE SPECIFIC
+	for _, loc := range known_callback_locations {
+		backing_dmt := qse.NewDMT[qse.WithId_H[string], qse.QuiverIndex]()
+		known_callbacks[loc] = dmtq.InsertNode(loc, &backing_dmt)
+	}
 	pc_chan := make(chan eidin.PathCondition)
+	// bgn EXAMPLE SPECIFIC
+	yes := true
+	fail_segment := eidin.PathConditionSegment{
+		ThisCallbackId: &eidin.CallbackId{
+			BytesStart: 170,
+			BytesEnd:   335,
+		},
+		NextCallbackId: &eidin.CallbackId{
+			BytesStart: 1,
+			BytesEnd:   1,
+		},
+		PartialPc: []*eidin.SMTConstraint{
+			&eidin.SMTConstraint{
+				Constraint:     "(= (*/read-var/* **jsvar_z) Y)",
+				AssertionValue: &yes,
+			},
+			&eidin.SMTConstraint{
+				Constraint:     "(*/read-var/* **jsvar_a)",
+				AssertionValue: &yes,
+			},
+		},
+	}
+	pc_chan <- eidin.PathCondition{
+		FreeFuns: []*eidin.SMTFreeFun{},
+		SegmentedPc: []*eidin.PathConditionSegment{
+			&fail_segment,
+		},
+	}
+	// end EXAMPLE SPECIFIC
 	go func() {}()
 }
