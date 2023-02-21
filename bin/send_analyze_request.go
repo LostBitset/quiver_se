02f69f96ee5b606e0518404795ff0ae48549ed1d
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-21 17:34:11 -0500

    Wrote `SendAnalyzeRequest`

--- bin/send_analyze_request.go
+++ bin/send_analyze_request.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	eidin "LostBitset/quiver_se/EIDIN/proto_lib"
+	"fmt"
+	"hash/fnv"
+	"os"
+
+	"google.golang.org/protobuf/proto"
+)
+
 func SendAnalyzeRequest(msg_prefix string, model string) {
-	// TODO
+	message := eidin.Analyze{
+		ForbidCaching:  false,
+		Model:          &model,
+		SingleCallback: false,
+	}
+	msg_raw, proto_enc_err := proto.Marshal(&message)
+	if proto_enc_err != nil {
+		panic(proto_enc_err)
+	}
+	cwd, cwd_err := os.Getwd()
+	if cwd_err != nil {
+		panic(cwd_err)
+	}
+	hasher := fnv.New64a()
+	hasher.Write(msg_raw)
+	f, file_err := os.Create(
+		fmt.Sprintf(
+			"%s/../js_concolic/.eidin-run/Analyze/%s_%d_fromqse.eidin.bin",
+			cwd,
+			msg_prefix,
+			hasher.Sum64(),
+		),
+	)
+	if file_err != nil {
+		panic(file_err)
+	}
+	defer f.Close()
+	f.Write(msg_raw)
 }
