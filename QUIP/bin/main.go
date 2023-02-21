044bf67f184a860598d8b3e21cbcbb2fde7d1dac
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-20 19:20:39 -0500

    It's time

--- QUIP/bin/main.go
+++ QUIP/bin/main.go
@@ -8,10 +8,14 @@ import (
 
 func main() {
 	fmt.Println("[QUIP:(bin/)main.go] Started QUIP.")
-	if len(os.Args) != 3 {
+	if len(os.Args) != 2 {
 		panic("ERR! Need two arguments, filename and message prefix. ")
 	}
 	target := os.Args[1]
-	msg_prefix := os.Args[2]
+	msg_prefix := quip.GetMessagePrefix(target)
+	fmt.Println("[QUIP:(bin/)main.go] Performing initial instrumentation...")
+	quip.InstrumentFunctionInfo(target)
+	fmt.Println("[QUIP:(bin/)main.go] Initial instrumentation complete.")
+	fmt.Println("[QUIP:(bin/)main.go] Running all QUIP components...")
 	quip.StartQUIP(target, msg_prefix)
 }
