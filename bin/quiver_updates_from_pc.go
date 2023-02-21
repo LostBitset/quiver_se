1ff4294a3340ff8a6c5052f1714729f1a0511fd8
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-21 17:46:32 -0500

    Some form of progress

--- bin/quiver_updates_from_pc.go
+++ bin/quiver_updates_from_pc.go
@@ -20,5 +20,29 @@ func QuiverUpdatesFromPathCondition(
 		[]qse.SMTFreeFun[string, string],
 	],
 ) {
+	quiver_updates = make(
+		[]qse.Augmented[
+			qse.QuiverUpdate[
+				int,
+				qse.PHashMap[qse.Literal[qse.WithId_H[string]], struct{}],
+				*qse.DMT[qse.WithId_H[string], qse.QuiverIndex],
+			],
+			[]qse.SMTFreeFun[string, string],
+		],
+		0,
+	)
+	eidin_free_funs := pc.GetFreeFuns()
+	free_funs := make([]qse.SMTFreeFun[string, string], 0)
+	for _, eidin_free_fun := range eidin_free_funs {
+		eidin_free_fun := *eidin_free_fun
+		free_funs = append(
+			free_funs,
+			qse.SMTFreeFun[string, string]{
+				Name: eidin_free_fun.GetName(),
+				Args: eidin_free_fun.GetArgSorts(),
+				Ret:  eidin_free_fun.GetRetSort(),
+			},
+		)
+	}
 	// TODO
 }
