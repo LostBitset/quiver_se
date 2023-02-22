36d8e77e69af1340a7fe4f520d22d99ce0bdd9d4
Landon <102832670+LostBitset@users.noreply.github.com>
2023-02-21 20:21:55 -0500

    Not that

---
+++ bin/example_failure.js
@@ -0,0 +1,45 @@
+var EventEmitter = require("node:events");
+
+var ev = new EventEmitter();
+
+var sym__x = 2;
+var sym__y = 4;
+
+var z = sym__x;
+var a = false;
+
+
+
+function onFirst() {
+    if (z === sym__y + 1 && a) {
+        throw 'Stickerbrush? Really?';
+    }
+    if (z < sym__y) {
+        z = z + 2;
+        ev.emit("second");
+    }
+}
+
+function onSecond() {
+    if (z === sym__y && !a) {
+        ev.emit("third");
+    } else {
+        ev.emit("first");
+    }
+}
+
+function onThird() {
+    z = z - 1;
+    if (z != 2) {
+        a = true;
+    }
+    ev.emit("first");
+}
+
+ev.on("first", onFirst);
+ev.on("second", onSecond);
+ev.on("third", onThird);
+
+if (sym__x < sym__y) {
+    ev.emit("first");
+}
