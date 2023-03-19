205c79d9f5f132911ea1f803861508bcd9509bbb
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-19 11:46:42 -0400

    Something is broken

--- lib/simreq_part_real_test.go
+++ lib/simreq_part_real_test.go
@@ -3,6 +3,8 @@ package qse
 import (
 	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type SiMReQTestingQuiverIndexMap struct {
@@ -26,7 +28,7 @@ func MakeNodeWithCallbackForSiMReQTesting(
 	top_node QuiverIndex,
 	value int,
 	save_index *SiMReQTestingQuiverIndexMap,
-	wg sync.WaitGroup,
+	wg *sync.WaitGroup,
 ) (
 	update QuiverUpdate[
 		int,
@@ -34,6 +36,7 @@ func MakeNodeWithCallbackForSiMReQTesting(
 		*DMT[WithId_H[string], QuiverIndex],
 	],
 ) {
+	wg.Add(1)
 	update_dmt := NewDMT[WithId_H[string], QuiverIndex]()
 	intended_node := dmtq.NewIntendedNode(value, &update_dmt)
 	intended_node_cb_backing := func(index QuiverIndex) {
@@ -68,5 +71,251 @@ func TestSiMReQPartReal(t *testing.T) {
 	dmtq, top_node, fail_node, _ := StartSiMReQ[int, string, string, string, string, SMTLib2VAStringSolvedCtx](
 		in_updates, out_models, sys, nil,
 	)
-	nodes := []int{1, 2, 3}
+	nodes := []int{11, 12, 13}
+	index_map_container_backing := NewSiMReQTestingQuiverIndexMap()
+	index_map_container := &index_map_container_backing
+	var wg sync.WaitGroup
+	smt_free_funs := []SMTFreeFun[string, string]{
+		{"x", []string{}, "Int"},
+		{"y", []string{}, "Int"},
+	}
+	for _, node := range nodes {
+		in_updates <- Augmented[
+			QuiverUpdate[int, PHashMap[Literal[WithId_H[string]], struct{}], *DMT[WithId_H[string], QuiverIndex]],
+			[]SMTFreeFun[string, string],
+		]{
+			MakeNodeWithCallbackForSiMReQTesting(
+				dmtq,
+				top_node,
+				node,
+				index_map_container,
+				&wg,
+			),
+			smt_free_funs,
+		}
+	}
+	wg.Wait()
+	index_map := index_map_container.m
+	// begin edges
+	in_updates <- Augmented[
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		],
+		[]SMTFreeFun[string, string],
+	]{
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		]{
+			top_node,
+			dmtq.ParameterizeIndex(index_map[11]),
+			pto(StdlibMapToPHashMap(
+				map[Literal[WithId_H[string]]]struct{}{
+					{
+						WithId_H[string]{"@__RAW__(*/write-var/* **jsvar_z *{{x}}*)", idsrc.Gen()},
+						true,
+					}: {},
+					{
+						WithId_H[string]{"@__RAW__(*/write-var/* **jsvar_a *{{false}}*)", idsrc.Gen()},
+						true,
+					}: {},
+					{
+						WithId_H[string]{"(< (*/read-var/* **jsvar_z) y)", idsrc.Gen()},
+						true,
+					}: {},
+				},
+			)),
+		},
+		smt_free_funs,
+	}
+	in_updates <- Augmented[
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		],
+		[]SMTFreeFun[string, string],
+	]{
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		]{
+			index_map[11],
+			dmtq.ParameterizeIndex(index_map[12]),
+			pto(StdlibMapToPHashMap(
+				map[Literal[WithId_H[string]]]struct{}{
+					{
+						WithId_H[string]{"(< (*/read-var/* **jsvar_z) y)", idsrc.Gen()},
+						true,
+					}: {},
+					{
+						WithId_H[string]{"@__RAW__(*/write-var/* **jsvar_z *{{(+ (*/read-var/* **jsvar_z) 2)}}*)", idsrc.Gen()},
+						true,
+					}: {},
+				},
+			)),
+		},
+		smt_free_funs,
+	}
+	in_updates <- Augmented[
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		],
+		[]SMTFreeFun[string, string],
+	]{
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		]{
+			index_map[12],
+			dmtq.ParameterizeIndex(index_map[13]),
+			pto(StdlibMapToPHashMap(
+				map[Literal[WithId_H[string]]]struct{}{
+					{
+						WithId_H[string]{"(= (*/read-var/* **jsvar_z) y)", idsrc.Gen()},
+						true,
+					}: {},
+					{
+						WithId_H[string]{"(*/read-var/* **jsvar_a)", idsrc.Gen()},
+						false,
+					}: {},
+				},
+			)),
+		},
+		smt_free_funs,
+	}
+	in_updates <- Augmented[
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		],
+		[]SMTFreeFun[string, string],
+	]{
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		]{
+			index_map[12],
+			dmtq.ParameterizeIndex(index_map[11]),
+			pto(StdlibMapToPHashMap(
+				map[Literal[WithId_H[string]]]struct{}{
+					{
+						WithId_H[string]{"(not (and (= (*/read-var/* **jsvar_z) y) (not (*/read-var/* **jsvar_a))))", idsrc.Gen()},
+						false,
+					}: {},
+				},
+			)),
+		},
+		smt_free_funs,
+	}
+	in_updates <- Augmented[
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		],
+		[]SMTFreeFun[string, string],
+	]{
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		]{
+			index_map[13],
+			dmtq.ParameterizeIndex(index_map[11]),
+			pto(StdlibMapToPHashMap(
+				map[Literal[WithId_H[string]]]struct{}{
+					{
+						WithId_H[string]{"@__RAW__(*/write-var/* **jsvar_z *{{(- (*/read-var/* **jsvar_z) 1)}}*)", idsrc.Gen()},
+						true,
+					}: {},
+					{
+						WithId_H[string]{"(= (*/read-var/* **jsvar_z) 2)", idsrc.Gen()},
+						true,
+					}: {},
+				},
+			)),
+		},
+		smt_free_funs,
+	}
+	in_updates <- Augmented[
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		],
+		[]SMTFreeFun[string, string],
+	]{
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		]{
+			index_map[13],
+			dmtq.ParameterizeIndex(index_map[11]),
+			pto(StdlibMapToPHashMap(
+				map[Literal[WithId_H[string]]]struct{}{
+					{
+						WithId_H[string]{"@__RAW__(*/write-var/* **jsvar_z *{{(- (*/read-var/* **jsvar_z) 1)}}*)", idsrc.Gen()},
+						true,
+					}: {},
+					{
+						WithId_H[string]{"(= (*/read-var/* **jsvar_z) 2)", idsrc.Gen()},
+						false,
+					}: {},
+					{
+						WithId_H[string]{"@__RAW__(*/write-var/* **jsvar_a *{{true}}*)", idsrc.Gen()},
+						true,
+					}: {},
+				},
+			)),
+		},
+		smt_free_funs,
+	}
+	in_updates <- Augmented[
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		],
+		[]SMTFreeFun[string, string],
+	]{
+		QuiverUpdate[
+			int,
+			PHashMap[Literal[WithId_H[string]], struct{}],
+			*DMT[WithId_H[string], QuiverIndex],
+		]{
+			index_map[11],
+			dmtq.ParameterizeIndex(fail_node),
+			pto(StdlibMapToPHashMap(
+				map[Literal[WithId_H[string]]]struct{}{
+					{
+						WithId_H[string]{"(= (*/read-var/* **jsvar_z) (+ y 1))", idsrc.Gen()},
+						true,
+					}: {},
+					{
+						WithId_H[string]{"(*/read-var/* **jsvar_a)", idsrc.Gen()},
+						false,
+					}: {},
+				},
+			)),
+		},
+		smt_free_funs,
+	}
+	// end edges
+	close(in_updates)
+	models := make([]string, 0)
+	for model := range out_models {
+		models = append(models, model)
+	}
+	assert.NotEqual(t, len(models), 0)
 }
