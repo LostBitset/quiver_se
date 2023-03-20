a8ad54258a3cfefb9808f19999a7946177d061d4
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-20 15:32:11 -0400

    Add `ErdosRenyiQuiverGivenEdges`

---
+++ bin/erdos_renyi_quiver.go
@@ -0,0 +1,17 @@
+package main
+
+type SimpleQuiverAdjList struct {
+	adj_list []SimpleEdgeDesc
+}
+
+type SimpleEdgeDesc struct {
+	src int
+	dst int
+}
+
+type ConnectedOrNot int
+
+const (
+	Connected ConnectedOrNot = iota
+	NotConnected
+)
--- bin/erdos_renyi_quiver_impl.go
+++ bin/erdos_renyi_quiver_impl.go
@@ -1,25 +1,32 @@
 package main
 
-import (
-	qse "LostBitset/quiver_se/lib"
-)
-
-type SimpleQuiverAdjList struct {
-	adj_list []QuiverEdgeDesc
-}
-
-type QuiverEdgeDesc struct {
-	src qse.QuiverIndex
-	dst qse.QuiverIndex
-}
+import "math"
 
 // Generates a G(n, p) Erdős-Rényi Graph.
-func ErdosRenyiGraph(n int, p float64) (al SimpleQuiverAdjList)
+func ErdosRenyiGraph(n int, p float64) (al SimpleQuiverAdjList) {
+	adj_list := make([]SimpleEdgeDesc, 0)
+	edge_exists_distr := SimpleDDistr[ConnectedOrNot]{map[ConnectedOrNot]float64{
+		Connected:    p,
+		NotConnected: 1.0 - p,
+	}}
+	edge_exists := BakeDDistr[ConnectedOrNot](edge_exists_distr)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			// Self-loops are allowed
+			switch edge_exists.Sample() {
+			case Connected:
+				adj_list = append(adj_list, SimpleEdgeDesc{i, j})
+			}
+		}
+	}
+	al = SimpleQuiverAdjList{adj_list}
+	return
+}
 
 // Generates r G(n, p) Erdős-Rényi Graphs, and merges them to form
 // a quiver.
 func ErdosRenyiQuiver(n int, p float64, r int) (al SimpleQuiverAdjList) {
-	adj_list := make([]QuiverEdgeDesc, 0)
+	adj_list := make([]SimpleEdgeDesc, 0)
 	for i := 0; i < r; i++ {
 		adj_list = append(
 			adj_list,
@@ -27,5 +34,15 @@ func ErdosRenyiQuiver(n int, p float64, r int) (al SimpleQuiverAdjList) {
 		)
 	}
 	al = SimpleQuiverAdjList{adj_list}
+	return
+}
+
+// See ErdosRenyiQuiver.
+// Takes in n, p, and the average number of edges
+func ErdosRenyiQuiverGivenEdges(n int, p float64, avg_ne int) (al SimpleQuiverAdjList) {
+	n_choose_2 := (n * (n + 1)) / 2
+	r_float := float64(avg_ne) / (float64(n_choose_2) * p)
+	r := int(math.Round(r_float))
+	al = ErdosRenyiQuiver(n, p, r)
 	return
 }
