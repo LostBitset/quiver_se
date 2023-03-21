e3e2bb00d71f492e128d4db55100920cab7b4dad
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-21 16:59:10 -0400

    Time to write `SolveForInputZ3`

--- bin/dse_impl.go
+++ bin/dse_impl.go
@@ -1,8 +1,11 @@
 package main
 
-import "strings"
+import (
+	qse "LostBitset/quiver_se/lib"
+	"strings"
+)
 
-func (uprgm Microprogram) RunDSE() (n_bugs int) {
+func (uprgm Microprogram) RunDSE(idsrc *qse.IdSource) (n_bugs int) {
 	n_bugs = 0
 	model := uprgm.UnitializedAssignment()
 	imm_failure, imm_pc := uprgm.ExecuteGetPathCondition(model)
@@ -12,7 +15,7 @@ func (uprgm Microprogram) RunDSE() (n_bugs int) {
 	}
 	// The two main variables for the concolic execution algorithm:
 	alt_stack := make([]uint, 0)
-	desired_path := make([]string, 0)
+	desired_path := make([]qse.IdLiteral[string], 0)
 	// Set them up using the first path condition
 	stack_setup_index := 0
 setupAltStackAndPathLoop:
@@ -21,12 +24,50 @@ setupAltStackAndPathLoop:
 			continue setupAltStackAndPathLoop
 		}
 		alt_stack = append(alt_stack, uint(stack_setup_index))
-		desired_path = append(desired_path, constraint)
+		desired_path = append(
+			desired_path,
+			MicroprogramConstraintToIdLiteral(
+				constraint,
+				idsrc,
+			))
 		stack_setup_index++
 	}
 	// Main loop
+mainDSESearchAlternativesLoop:
 	for len(alt_stack) > 0 {
-		// TODO all of this
+		alt_stack_pop_index := len(alt_stack) - 1
+		inv_index := alt_stack[alt_stack_pop_index]
+		qse.SpliceOutReclaim(&alt_stack, alt_stack_pop_index)
+		orig_expr := desired_path[inv_index]
+		desired_path = desired_path[:inv_index]
+		desired_path = append(desired_path, qse.IdLiteral[string]{
+			Value: orig_expr.Value,
+			Eq:    !orig_expr.Eq,
+		})
+		new_model_ptr := uprgm.SolveForInputZ3(desired_path)
+		if new_model_ptr == nil {
+			continue mainDSESearchAlternativesLoop
+		}
+		new_model := *new_model_ptr
+		fails, pc := uprgm.ExecuteGetPathCondition(new_model)
+		if fails {
+			n_bugs++
+		}
+		desired_path = make([]qse.IdLiteral[string], len(pc))
+		for i, path_condition_elem := range pc {
+			desired_path[i] = MicroprogramConstraintToIdLiteral(
+				path_condition_elem,
+				idsrc,
+			)
+		}
+	updateAltStackGivenDiscoveredPathLoop:
+		for index := inv_index + 1; index < uint(len(desired_path)); index++ {
+			curr_path_item := desired_path[index]
+			if strings.HasPrefix(curr_path_item.Value.Value, "@__RAW__;;@RICHPC:") {
+				continue updateAltStackGivenDiscoveredPathLoop
+			}
+			alt_stack = append(alt_stack, index)
+		}
 	}
 	return
 }
