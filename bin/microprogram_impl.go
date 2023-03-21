47c59a5d8d5b4773e8c074d9fc36f9a74c9ff60b
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-20 21:47:46 -0400

    Add some special cases (0 and 1 dst), time to test

--- bin/microprogram_impl.go
+++ bin/microprogram_impl.go
@@ -44,15 +44,31 @@ func (gen *MicroprogramGenerator) RandomMicroprogram() (uprgm Microprogram) {
 	// Replace edges with random trees of random constraints
 	adj_list_map := base_quiver.ExtractAdjListAsMap(n_nodes)
 	uprgm_transitions := make(map[MicroprogramState][]MicroprogramTransition)
+buildUpUprgmTransitionsLoop:
 	for src, dst_list := range adj_list_map {
-		n_branches := len(dst_list)
-		tree := PruferEvenFinalRandomTree(gen.n_tree_nonleaf, n_branches)
-		src_state := node_allocation.ShiftBy(src)
-		dst_states := make([]MicroprogramState, n_branches)
-		for i, dst := range dst_list {
-			dst_states[i] = node_allocation.ShiftBy(dst)
+		if len(dst_list) == 0 {
+			continue buildUpUprgmTransitionsLoop
 		}
-		new_transitions := tree.AsMicroprogramTransitions(dst_states, gen.constraintgen)
+		var new_transitions []MicroprogramTransition
+		src_state := node_allocation.ShiftBy(src)
+		if len(dst_list) == 1 {
+			new_transitions = []MicroprogramTransition{
+				{
+					dst_state: node_allocation.ShiftBy(dst_list[0]),
+					constraints: []string{
+						gen.constraintgen.Generate(BoolSort),
+					},
+				},
+			}
+		} else {
+			n_branches := len(dst_list)
+			tree := PruferEvenFinalRandomTree(gen.n_tree_nonleaf, n_branches)
+			dst_states := make([]MicroprogramState, n_branches)
+			for i, dst := range dst_list {
+				dst_states[i] = node_allocation.ShiftBy(dst)
+			}
+			new_transitions = tree.AsMicroprogramTransitions(dst_states, gen.constraintgen)
+		}
 		uprgm_transitions[src_state] = append(uprgm_transitions[src_state], new_transitions...)
 	}
 	uprgm = Microprogram{
---
+++ bin/microprograms_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	qse "LostBitset/quiver_se/lib"
+	"fmt"
+	"testing"
+)
+
+const TEST_ITERATIONS_MICROPROGRAMS = TEST_ITERATIONS * 2
+
+func TestMicroprogramGeneration(t *testing.T) {
+	ops, vals := GetStandardItems()
+	constraint_gen := ConstraintGenerator{
+		n_depth_mean:   2.0,
+		n_depth_stddev: 1.5,
+		ops:            ops,
+		vals:           vals,
+		next_var_id:    pto(0),
+	}
+	var_sorts := SimpleDDistr[Sort]{
+		map[Sort]float64{
+			RealSort: 0.7,
+			BoolSort: 0.3,
+		},
+	}
+	var_sorts_distr := BakeDDistr[Sort](var_sorts)
+	constraint_gen.AddVariables(4, var_sorts_distr, 0.75)
+	uprgm_gen := MicroprogramGenerator{
+		n_states:          7,
+		p_transition:      0.75,
+		avg_n_transitions: 2.0,
+		p_fallible:        0.4,
+		n_entry_samples:   3,
+		n_tree_nonleaf:    4,
+		constraintgen:     constraint_gen,
+		smt_free_funs:     []qse.SMTFreeFun[string, string]{},
+	}
+	fmt.Printf("Generating %d random microprograms.\n", TEST_ITERATIONS_MICROPROGRAMS)
+	for i := 0; i < TEST_ITERATIONS_MICROPROGRAMS; i++ {
+		uprgm_gen.RandomMicroprogram()
+	}
+}
