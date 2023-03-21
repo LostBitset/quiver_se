814f50fce79a490dbdd582d20645a69c09e1953f
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-21 10:33:15 -0400

    Seems correct

--- bin/path_conditions_test.go
+++ bin/path_conditions_test.go
@@ -1,7 +1,23 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+const TEST_ITERATIONS_PC = TEST_ITERATIONS / 16
 
 func TestPathConditions(t *testing.T) {
-	model1 = "(define-fun)"
+	initial_model := `
+	(define-fun var_0 () Real 0)
+	(define-fun var_1 () Real 0)
+	(define-fun var_2 () Real 0)
+	(define-fun var_3 () Real 0)
+	`
+	uprgm_gen := BuildTestingMicroprogramGenerator()
+	fmt.Printf("Computing path conditions for %d random microprograms.\n", TEST_ITERATIONS_PC)
+	for i := 0; i < TEST_ITERATIONS_PC; i++ {
+		uprgm := uprgm_gen.RandomMicroprogram()
+		uprgm.ExecuteGetPathCondition(initial_model)
+	}
 }
