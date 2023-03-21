b775ae63bc4113ee911660e52b21cee9ee935275
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-21 07:56:03 -0400

    Going to mess with generation again *sigh*

--- bin/path_conditions.go
+++ bin/path_conditions.go
@@ -1,5 +1,7 @@
 package main
 
+import qse "LostBitset/quiver_se/lib"
+
 func (uprgm Microprogram) ExecuteGetPathCondition(model string) (pc []string) {
 	pc = uprgm.ExecuteGetPathConditionFrom(model, uprgm.top_state)
 	return
@@ -23,4 +25,22 @@ selectTransitionLoop:
 		}
 	}
 	return
+}
+
+func (uprgm Microprogram) ModelSatisfiesConstraints(model string, constraints []string) (does bool) {
+	complete_query := make([]string, len(constraints)+1)
+	for i := range complete_query {
+		if i == 0 {
+			complete_query[i] = "@__RAW__" + model
+		} else {
+			complete_query[i] = constraints[i-1]
+		}
+	}
+	var idsrc qse.IdSource
+	complete_query_with_ids := make([]qse.IdLiteral[string], len(complete_query))
+	for i, part := range complete_query {
+		complete_query_with_ids[i] = Wi
+	}
+	sys := qse.SMTLibv2StringSystem{idsrc}
+	sys.CheckSat(complete_query)
 }
