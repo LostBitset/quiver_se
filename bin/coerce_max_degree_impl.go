24c5906684264bb2753566be6d03e7e08e4eff60
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-20 22:37:56 -0400

    Add recursion bit, still a TODO

--- bin/coerce_max_degree_impl.go
+++ bin/coerce_max_degree_impl.go
@@ -1,5 +1,11 @@
 package main
 
 func (tree SimpleTree) CoerceToMaxDegree(n int) {
-	// TODO
+	if len(tree.children) > n {
+		// Put all of the other children in their own tree
+		// TODO
+	}
+	for _, child := range tree.children {
+		child.CoerceToMaxDegree(n)
+	}
 }
