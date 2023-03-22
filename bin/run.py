c62e7315bfbf872a4727fa9c6521d71390b77d5f
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-22 00:24:34 -0400

    Ready to run :)

--- bin/run.py
+++ bin/run.py
@@ -1,4 +1,6 @@
+import itertools
 import subprocess
+import sys
 
 def run_once():
     cmd = "go run ."
@@ -12,10 +14,46 @@ def run_once():
     if "exit status 2" in output:
         return None
 
+    n_bugs = {"dse": 0, "simreq": 0}
+    n_sq = {"dse": 0, "simreq": 0}
+    n_exec = {"dse": 0, "simreq": 0}
     target = None
     for line in output.split("\n"):
-        print(line)
+        print(line, file=sys.stderr)
         if not line.startswith("[REPORT]"):
             continue
-        
+        evaluating = "[REPORT] [EVALUATING]"
+        if line.startswith(evaluating):
+            target = line[len(evaluating)+1:]
+        elif "__FOUND_A_BUG__dse" in line:
+            n_bugs["dse"] += 1
+        elif "__FOUND_A_BUG__simreq" in line:
+            n_bugs["simreq"] += 1
+        elif "[EVAL-INFO] SOLVERQUERY" in line:
+            n_sq[target] += 1
+        elif "[EVAL-INFO] EXECUTION" in line:
+            n_exec[target] += 1
+    if n_bugs['dse'] == n_bugs['simreq']:
+        if n_bugs['dse'] == 0:
+            return None
+    return {"bugs": n_bugs, "queries": n_sq, "executions": n_exec}
+
+total_bugs = {"dse": 0, "simreq": 0}
+total_queries = {"dse": 0, "simreq": 0}
+total_execs = {"dse": 0, "simreq": 0}
+n_samples = 0
+for i in itertools.count():
+    r = run_once()
+    if r == None:
+        continue
+    n_samples += 1
+    for alg in ["dse", "simreq"]:
+        total_bugs[alg] += r["bugs"][alg]
+        total_queries[alg] += r["queries"][alg]
+        total_execs[alg] += r["executions"][alg]
+    print("\tDSE\tSiMReQ")
+    print(f"BUGS\t{total_bugs['dse']/n_samples:.2f}\t{total_bugs['simreq']/n_samples:.2f}")
+    print(f"QRIS\t{total_queries['dse']/n_samples:.2f}\t{total_queries['simreq']/n_samples:.2f}")
+    print(f"EXCS\t{total_execs['dse']/n_samples:.2f}\t{total_execs['simreq']/n_samples:.2f}")
+    print("--- --- ---")
 
