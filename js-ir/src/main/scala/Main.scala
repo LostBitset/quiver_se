09bfa46ef7ce60b0639690e0923675d1db327223
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-05 10:52:51 -0400

    There

--- js-ir/src/main/scala/Main.scala
+++ js-ir/src/main/scala/Main.scala
@@ -1,23 +1,38 @@
 @main def hello: Unit =
   println("Nothing here yet. ")
 
-class IrValue[T : IrType](c: IrConstruct[T])
-
-enum IrConstruct[+T : IrType]:
-  case Const[T: IrType](value: T)     extends IrConstruct[T]
-  case Array(elems: List[IrValue[?]]) extends IrConstruct[IrArray]
-
 enum IrExpr[+R : IrType]:
-  case Value[R : IrType](repr: IrValue[R]) extends IrExpr[R]
-  case FCall[R : IrType](func: IrExpr[IrFunc[R]], args: List[IrValue[?]]) extends IrExpr[R]
+  case Value[R : IrType](repr: R)                                    extends IrExpr[R]
+  case FCall[R : IrType](func: IrExpr[IrFunc[R]], args: List[IrAny]) extends IrExpr[R]
+  case Var[R : IrType](name: String)                                 extends IrExpr[R]
+
+type IrAny = IrWrapped[?]
+class IrWrapped[T : IrType](value: T)
+
+enum IrStmt[+R : IrType]:
+  case Expr[V](expr: IrExpr[V])          extends IrStmt[Nothing]
+  case Retn[R : IrType](expr: IrExpr[R]) extends IrStmt[R]
+
+type IrBody[+R] = List[IrStmt[R]]
 
 trait IrType[A]
 
-class IrFunc[+R]
-given IrFuncIsType[R]: IrType[IrFunc[R]] with {}
+given BottomIsType: IrType[Nothing]                     with {}
 
-class IrArray
-given IrArrayIsType: IrType[IrArray] with {}
+class IrFunc[+R](body: IrBody[R], args: List[String])
+given IrFuncIsType[R]: IrType[IrFunc[R]]                with {}
+
+class IrArray(backing: List[IrAny])
+given IrArrayIsType: IrType[IrArray]                    with {}
+
+class IrNumber(n: Int | Double)
+given IrNumberIsType: IrType[IrNumber]                  with {}
+
+class IrBool(b: Boolean)
+given IrBoolIsType: IrType[IrBool]                      with {}
+
+class IrString(s: String)
+given IrStringIsType: IrType[IrString]                  with {}
 
 class Shadow[+R : IrType, +S](value: R, shadow: S)
 given ShadowIsType[R : IrType, S]: IrType[Shadow[R, S]] with {}
