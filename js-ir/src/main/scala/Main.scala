596e224ff0959a97ed53bdb4e19aad072668f20e
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-05 10:12:55 -0400

    No good

--- js-ir/src/main/scala/Main.scala
+++ js-ir/src/main/scala/Main.scala
@@ -1,2 +1,14 @@
 @main def hello: Unit =
   println("Nothing here yet. ")
+
+trait IrType[V]:
+  def resolveMethod[R](method: String): IrFunc[R]
+
+enum IrExpr[+V]:
+  case Const[V : IrType](value: V)                            extends IrExpr[V]
+  case Var[V: IrType](value: String)                          extends IrExpr[V]
+  case Call[R : IrType](fn: IrFunc[R], args: List[IrExpr[?]]) extends IrExpr[R]
+
+type IrFunc[+R] = IrExpr[IrLambda[R]]
+
+class IrLambda[+R : IrType]
