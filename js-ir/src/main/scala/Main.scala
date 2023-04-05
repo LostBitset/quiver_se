bf311007737870cfb86f33f70ac9a6a25e30a849
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-05 10:33:53 -0400

    Need a better way to handle shadows

--- js-ir/src/main/scala/Main.scala
+++ js-ir/src/main/scala/Main.scala
@@ -1,17 +1,23 @@
 @main def hello: Unit =
   println("Nothing here yet. ")
 
-class IrValue[T <: IrType](c: IrConstruct[T])
+class IrValue[T : IrType](c: IrConstruct[T])
 
-enum IrConstruct[+T <: IrType]:
-  case Const(value: T)                extends IrConstruct[T]
+enum IrConstruct[+T : IrType]:
+  case Const[T: IrType](value: T)     extends IrConstruct[T]
   case Array(elems: List[IrValue[?]]) extends IrConstruct[IrArray]
 
-enum IrExpr[+R <: IrType]:
-  case Value[R <: IrType](repr: IrValue[R]) extends IrExpr[R]
-  case FCall[R <: IrType](func: IrExpr[IrFunc[R]], args: List[IrValue[?]]) extends IrExpr[R]
+enum IrExpr[+R : IrType]:
+  case Value[R : IrType](repr: IrValue[R]) extends IrExpr[R]
+  case FCall[R : IrType](func: IrExpr[IrFunc[R]], args: List[IrValue[?]]) extends IrExpr[R]
 
-sealed trait IrType
+trait IrType[A]
 
-class IrFunc[+R] extends IrType
-class IrArray    extends IrType
+class IrFunc[+R]
+given IrFuncIsType[R]: IrType[IrFunc[R]] with {}
+
+class IrArray
+given IrArrayIsType: IrType[IrArray] with {}
+
+class Shadow[+R : IrType, +S](value: R, shadow: S)
+given ShadowIsType[R : IrType, S]: IrType[Shadow[R, S]] with {}
