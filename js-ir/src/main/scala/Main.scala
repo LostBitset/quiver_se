22d253533a678339fe6e64aab7e1f8f2ae2f69a0
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-05 11:10:51 -0400

    There, conversions

--- js-ir/src/main/scala/Main.scala
+++ js-ir/src/main/scala/Main.scala
@@ -7,7 +7,7 @@ enum IrExpr[+R : IrType]:
   case Var[R : IrType](name: String)                                 extends IrExpr[R]
 
 type IrAny = IrWrapped[?]
-class IrWrapped[T : IrType](value: T)
+case class IrWrapped[T : IrType](value: T)
 
 enum IrStmt[+R : IrType]:
   case Expr[V](expr: IrExpr[V])          extends IrStmt[Nothing]
@@ -26,25 +26,29 @@ enum IrFunc[+R : IrType]:
 
 given IrFuncIsType[R]: IrType[IrFunc[R]]                                  with {}
 
-class IrArray(backing: List[IrAny])
+case class IrArray(backing: List[IrAny])
 given IrArrayIsType: IrType[IrArray]                                      with {}
 
-class IrNumber(n: Int | Double)
+case class IrNumber(n: Int | Double)
 given IrNumberIsType: IrType[IrNumber]                                    with {}
 
-class IrBool(b: Boolean)
+case class IrBool(b: Boolean)
 given IrBoolIsType: IrType[IrBool]                                        with {}
 
-class IrString(s: String)
+case class IrString(s: String)
 given IrStringIsType: IrType[IrString]                                    with {}
 
-class Shadow[+R : IrType, +S : ShadowVBound[R]](value: R, shadow: S)
+case class Shadow[+R : IrType, +S : ShadowVBound[R]](value: R, shadow: S)
 given ShadowIsType[R : IrType, S : ShadowVBound[R]]: IrType[Shadow[R, S]] with {}
 
 type ShadowVBound = [R] =>> [S] =>> ShadowV[S, R]
 trait ShadowV[+S, -R : IrType]:
   def basicForm(value: R): S
 
-given IrToShadow[R : IrType, S : ShadowVBound[R]]: Conversion[R, Shadow[R, S]] with
+given IrToShadow[R : IrType, S : ShadowVBound[R]]: Conversion[R, Shadow[R, S]]   with
   def apply(from: R): Shadow[R, S] =
     Shadow(from, summon[ShadowV[S, R]].basicForm(from))
+
+given IrFromShadow[R : IrType, S : ShadowVBound[R]]: Conversion[Shadow[R, S], R] with
+  def apply(from: Shadow[R, S]): R =
+    from.value
