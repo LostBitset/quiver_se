df82186aaf4cb8ddd956b36367a1cd50c2013dd3
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-13 15:23:43 -0400

    write test and start actual ir

--- js-ir/src/main/scala/Main.scala
+++ js-ir/src/main/scala/Main.scala
@@ -1,55 +1,17 @@
 @main def hello: Unit =
   println("Nothing here yet. ")
 
-enum IrExpr[+R : IrType]:
-  case Value[R : IrType](repr: R)                                    extends IrExpr[R]
-  case FCall[R : IrType](func: IrExpr[IrFunc[R]], args: List[IrAny]) extends IrExpr[R]
-  case Var[R : IrType](name: String)                                 extends IrExpr[R]
+case class SeirVal(repr: Any)
 
-type IrAny = IrWrapped[?]
-case class IrWrapped[T : IrType](value: T)
+type SeirIdent = String
 
-enum IrStmt[+R : IrType]:
-  case Expr[V](expr: IrExpr[V])               extends IrStmt[Nothing]
-  case Decl(name: String)                     extends IrStmt[Nothing]
-  case Bind[V](expr: IrExpr[V], name: String) extends IrStmt[Nothing]
-  case Retn[R : IrType](expr: IrExpr[R])      extends IrStmt[R]
+enum SeirExpr:
+  case Re(value: SeirVal)
+  case Scope(of: List[SeirExpr])
+  case Decl(name: SeirIdent)
+  case Def(name: SeirIdent, to: SeirExpr)
+  case Var(name: SeirIdent)
+  case Prop(obj: SeirExpr, property: SeirIdent)
+  case Call(f: SeirExpr, args: List[SeirExpr])
+  case Hidden(str: String)
 
-type IrBody[+R] = List[IrStmt[R]]
-
-trait IrType[A]
-
-given BottomIsType: IrType[Nothing]                                       with {}
-
-enum IrFunc[+R : IrType]:
-  case Constr[R : IrType](body: IrBody[R], args: List[String]) extends IrFunc[R]
-  case Lambda[R : IrType](body: IrBody[R], args: List[String]) extends IrFunc[R]
-  case Builtin(name: String)                                   extends IrFunc[Nothing]
-
-given IrFuncIsType[R]: IrType[IrFunc[R]]                                  with {}
-
-case class IrArray(backing: List[IrAny])
-given IrArrayIsType: IrType[IrArray]                                      with {}
-
-case class IrNumber(n: Int | Double)
-given IrNumberIsType: IrType[IrNumber]                                    with {}
-
-case class IrBool(b: Boolean)
-given IrBoolIsType: IrType[IrBool]                                        with {}
-
-case class IrString(s: String)
-given IrStringIsType: IrType[IrString]                                    with {}
-
-case class Shadow[+R : IrType, +S : ShadowVBound[R]](value: R, shadow: S)
-given ShadowIsType[R : IrType, S : ShadowVBound[R]]: IrType[Shadow[R, S]] with {}
-
-type ShadowVBound = [R] =>> [S] =>> ShadowV[R, S]
-trait ShadowV[-R : IrType, +S]:
-  def basicForm(value: R): S
-
-given IrToShadow[R : IrType, S : ShadowVBound[R]]: Conversion[R, Shadow[R, S]]   =
-  (from: R) =>
-    Shadow(from, summon[ShadowV[R, S]].basicForm(from))
-
-given IrFromShadow[R : IrType, S : ShadowVBound[R]]: Conversion[Shadow[R, S], R] =
-  _.value
--- js-ir/src/test/scala/MySuite.scala
+++ js-ir/src/test/scala/MySuite.scala
@@ -5,3 +5,50 @@ class TestSuite extends munit.FunSuite:
   test("sanity check") {
     assertEquals(1 + 1, 2)
   }
+
+
+  test("parses tokens") {
+    val text = """
+    |(scope
+    |  (decl x)
+    |  (decl y)
+    |  (def x {int 1})
+    |  (def y (.+ x x))
+    |  (.if (.= y {int 3})
+    |    (hidden <console.log("true")>)
+    |    (hidden <console.log("false")>)))
+    """.stripMargin
+    val parser = SeirParser(text)
+    val parsed = parser.takeExpr()
+    assertEquals(
+      parsed,
+      SeirExpr.Scope(
+        List(
+          SeirExpr.Decl("x"),
+          SeirExpr.Decl("y"),
+          SeirExpr.Def("x", SeirExpr.Re(SeirVal(1))),
+          SeirExpr.Def(
+            "y",
+            SeirExpr.Call(
+              SeirExpr.Var("+"),
+              List(SeirExpr.Var("x"), SeirExpr.Var("x"))
+            )
+          ),
+          SeirExpr.Call(
+            "if",
+            List(
+              SeirExpr.Call(
+                SeirExpr.Var("="),
+                List(
+                  SeirExpr.Var("y"),
+                  SeirExpr.Re(SeirVal(1))
+                )
+              ),
+              SeirExpr.Hidden("console.log(\"true\")"),
+              SeirExpr.Hidden("console.log(\"false\")"),
+            )
+          )
+        )
+      )
+    )
+  }
