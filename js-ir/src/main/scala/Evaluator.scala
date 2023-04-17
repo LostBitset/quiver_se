26a30d5688be2f055061435e87369249afc98b4a
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-17 12:19:52 -0400

    just scope and hidden left

--- js-ir/src/main/scala/Evaluator.scala
+++ js-ir/src/main/scala/Evaluator.scala
@@ -20,9 +20,13 @@ case class SeirEvaluator(
         expr match
             case SeirExpr.Re(value) => value
             case SeirExpr.Scope(of) => ???
-            case SeirExpr.Decl(name) => ???
-            case SeirExpr.Def(name, to) => ???
-            case SeirExpr.Var(name) => ???
+            case SeirExpr.Decl(name) =>
+                env.declare(name)
+                SeirVal(())
+            case SeirExpr.Def(name, to) =>
+                env.define(name, eval(to))
+                SeirVal(())
+            case SeirExpr.Var(name) => env(name)
             case call: SeirExpr.Call => apply(call)
             case SeirExpr.Hidden(str) => ???
             case SeirExpr.Capture(expr) => SeirVal(QuotedCapture(expr))
