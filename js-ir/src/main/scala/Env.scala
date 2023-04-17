a1b5f24040c56b7c77e3fadac210cae0a15245fb
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-17 11:59:07 -0400

    ready to test `SeirEnv`

--- js-ir/src/main/scala/Env.scala
+++ js-ir/src/main/scala/Env.scala
@@ -3,13 +3,24 @@ import scala.collection.mutable.{
 }
 
 class SeirEnv(
-    var stack: MutList[MutSet[String]], // which vars are defined in a given frame
-    var vars: MutMap[String, MutList[SeirVal]] // the definitions of a var
+    var stack: MutList[MutSet[String]] = MutList(), // which vars are defined in a given frame
+    var vars: MutMap[String, MutList[SeirVal]] = MutMap() // the definitions of a var
 ):
     def enterScope: Unit =
         stack.addOne(MutSet.empty)
     
     def leaveScope: Unit =
-        stack.dropRightInPlace(1)(0).foreach({
+        stack.dropRightInPlace(1).last.foreach({
             vars(_).dropRightInPlace(1)
         })
+
+    def declare(key: String): Unit =
+        stack.last.add(key)
+    
+    // Linear time for now, but this shouldn't really be a problem
+    def define(key: String, value: SeirVal): Unit =
+        val lastIndex = vars.size - 1
+        vars(key).patchInPlace(lastIndex, List(value), 1)
+
+    def apply(key: String): SeirVal =
+        vars(key).last
