071931ae544bd6216e5519e51f4243e63eadd19a
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-19 18:22:18 -0400

    added path condition stuff, time to test

--- js-ir/src/main/scala/ShadowHandles.scala
+++ js-ir/src/main/scala/ShadowHandles.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.{ListBuffer => MutList}
+
 given ShadowHandles = ShadowHandles(
     TiedMap[ShadowOpSpec]()
         + (ShadowOpSpec("smt", ShadowOp.Promote), repr => {
@@ -8,5 +10,20 @@ given ShadowHandles = ShadowHandles(
         + (ShadowOpSpec("smt", ShadowOp.Named("+")), (args, ctx) => {
             val spaceSep = args.map(_.shadow).asInstanceOf[List[String]].mkString(" ")
             s"(+ ${spaceSep})"
+        })
+        + (ShadowOpSpec("smt", ShadowOp.Named("if")), (args, ctx) => {
+            val cVal = args(0).repr.asInstanceOf[Boolean]
+            val cSym = args(0).shadow.asInstanceOf[String]
+            val pathCondition =
+                if cVal then
+                    cSym
+                else
+                    s"(not ${cSym})"
+            if !(ctx.map contains "path-cond") then
+                ctx.map += ("path-cond", MutList.empty[String])
+            ctx.map("path-cond")
+                .asInstanceOf[MutList[String]]
+                .addOne(pathCondition)
+            if cVal then args(1).shadow else args(2).shadow
         })
 )
