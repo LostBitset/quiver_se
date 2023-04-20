c9f8b442a0a5a559badd4990455d72237c970ea3
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-20 18:36:29 -0400

    saving work again (nh)

--- js-ir/src/main/scala/SegmentedPathCond.scala
+++ js-ir/src/main/scala/SegmentedPathCond.scala
@@ -18,17 +18,39 @@ def extractSPC(evaluator: SeirEvaluator): SegmentedPathCond =
             .map("path-cond")
             .asInstanceOf[MutList[String]]
             .toList
-    ???
+    SegmentedPathCond(
+        splitGroups(pc, SeirCallbackRef.Top)(item =>
+            if item.startsWith(SMT_EVTRXN_MAGIC) then
+                Some(item.drop(SMT_EVTRXN_MAGIC.length))
+            else
+                None
+        )
+            .map({
+                case (callback: SeirCallbackRef, constraintStrings) =>
+                    PathCondSegment(
+                        callback,
+                        constraintStrings.map(decodePathCondItem)
+                    )
+            })
+    )
 
-def splitGroups[A, B](list: List[A], f: A => Option[B], start: B): Map[B, List[A]] =
-  @tailrec
-  def loop(acc: Map[B, List[A]], rest: List[A], group: List[A], key: B): Map[B, List[A]] =
-    rest match
-      case Nil => acc + (key -> group.reverse)
-      case x :: xs =>
-        f(x) match
-            case None =>
-                loop(acc, xs, x :: group, key)
-            case Some(key) =>
-                loop(acc + (key -> group.reverse), xs, List.empty, key)
-  loop(Map(start -> List.empty), list, List.empty, start)
+def decodePathCondItem(s: String): PathCondItem =
+    
+
+// -- begin nh
+def splitGroups[A, B](list: List[A], start: B)(f: A => Option[B]): List[(B, List[A])] = {
+  // A helper function to accumulate groups into a list of pairs
+  @annotation.tailrec
+  def loop(acc: List[(B, List[A])], rest: List[A], currentGroup: List[A], currentKey: B): List[(B, List[A])] = {
+    rest match {
+      case Nil => acc :+ (currentKey, currentGroup.reverse) // Reverse the last group and add it to the accumulator
+      case x :: xs => f(x) match {
+        case None => loop(acc, xs, x :: currentGroup, currentKey) // Keep accumulating the current group
+        case Some(key) => loop(acc :+ (currentKey, currentGroup.reverse), xs, List.empty, key) // Start a new group with the new key
+      }
+    }
+  }
+
+  loop(List(), list, List.empty, start)
+}
+// -- end nh
