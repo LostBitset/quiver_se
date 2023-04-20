48f940ad26ab8233f1ad2425e1e2c57a164da0fd
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-20 17:32:37 -0400

    saving work

--- js-ir/src/main/scala/SegmentedPathCond.scala
+++ js-ir/src/main/scala/SegmentedPathCond.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.{ListBuffer => MutList}
+
 case class SegmentedPathCond(segments: List[PathCondSegment])
 
 case class PathCondSegment(callback: SeirCallbackRef, constraint: List[PathCondItem])
@@ -9,4 +11,15 @@ enum SeirCallbackRef:
     case Top
 
 def extractSPC(evaluator: SeirEvaluator): SegmentedPathCond =
-    ???
+    val pc =
+        evaluator
+            .shadowCtx
+            .map("path-cond")
+            .asInstanceOf[MutList[String]]
+            .toList
+    PathCondSegment(
+        segmentPC(pc, SeirCallbackRef.Top)
+    )
+
+def segmentPC(pc: List[String], entryCallback: SeirCallbackRef): List[PathCondSegment] =
+    
