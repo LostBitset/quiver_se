ebd01981c477a4c44fef227075cc81ef6b628e7b
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-21 11:10:01 -0400

    close but constraints have no variables

--- js-ir/src/main/scala/Main.scala
+++ js-ir/src/main/scala/Main.scala
@@ -1,10 +1,9 @@
 import grapple.json.{_, given}
 
-import scala.io.Source
 import scala.language.implicitConversions
 
-@main def main: Unit =
-    val input = Source.stdin.mkString("\n")
+@main def main(filename: String): Unit =
+    val input = io.Source.fromFile(filename).mkString
     val query = Json.parse(input).as[ConcolicQuery]
     val result = query.run
     val resultJson = Json.toJson(result)
