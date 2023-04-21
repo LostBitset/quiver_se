05bc90445d0b502e3f16288b2bd1bdf1a461712d
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-21 10:14:20 -0400

    Time to write `toExpr` function

--- js-ir/src/main/scala/ConcolicQuery.scala
+++ js-ir/src/main/scala/ConcolicQuery.scala
@@ -2,7 +2,14 @@ case class ConcolicQuery(
     languages: ConcolicQueryLanguages,
     vars: List[ConcolicVarDesc],
     source: String
-)
+):
+    def run: SegmentedPathCond =
+        val exprNoContext = SeirParser(source).takeExpr.get
+        val symPrelude = SeirPrelude(vars.map(_.toExpr))
+        val expr = symPrelude.transform(exprNoContext)
+        val evaluator = SeirEvaluator()
+        evaluator.evalSeir(expr)
+        extractSPC(evaluator)
 
 case class ConcolicQueryLanguages(smt: String, source: String)
 
@@ -10,7 +17,7 @@ class UnrecognizedSmtSort(sort: String)
     extends Exception(s"Unrecognized sort $sort")
 
 case class ConcolicVarDesc(smt_name: String, value: String, sort: String, source_name: String):
-    def asSeirValue: SeirVal = SeirVal(
+    def getAsSeirValue: SeirVal = SeirVal(
         sort match
             case "Int" => value.toInt
             case _ => throw UnrecognizedSmtSort(sort)
