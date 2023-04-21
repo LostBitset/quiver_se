eb3e8bc5b9626a08eb3554f974d43d5a2a70ad26
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-21 10:19:28 -0400

    json output time

--- js-ir/src/main/scala/Main.scala
+++ js-ir/src/main/scala/Main.scala
@@ -1,2 +1,7 @@
+import grapple.json.{_, given}
+
 @main def main: Unit =
+    val input = Console.in.readLine
+    val query = Json.parse(input).as[ConcolicQuery]
+    val result = query.run
     ???
