c33867c681d18550bec64f5d8490622bfb851b59
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-21 21:23:54 -0400

    Just need to write to a file

--- quiver-gviz/main.go
+++ quiver-gviz/main.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"fmt"
-
-	qse "github.com/LostBitset/quiver_se/lib"
-	synth "github.com/LostBitset/quiver_se/lib_synthetic"
-)
-
-type _Hold1 = qse.QuiverIndex
-type _Hold2 = synth.ConnectedOrNot
+import "fmt"
 
 func main() {
-	fmt.Println("Nothing here yet.")
+	uprgm := GenerateEvaluationMicroprogram()
+	dot := MicroprogramQuiverDot(uprgm)
+	fmt.Println(dot.String())
 }
