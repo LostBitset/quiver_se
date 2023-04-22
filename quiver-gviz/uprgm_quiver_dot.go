db538471373ea7c7841b1dc16e5018b3f2027999
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-21 21:20:27 -0400

    more `MicroprogramQuiverDot` work

--- quiver-gviz/uprgm_quiver_dot.go
+++ quiver-gviz/uprgm_quiver_dot.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"fmt"
+
 	s "github.com/LostBitset/quiver_se/lib_synthetic"
 	"github.com/emicklei/dot"
 )
 
+type GenEdge[A comparable] struct {
+	Src A
+	Dst A
+}
+
 func MicroprogramQuiverDot(uprgm s.Microprogram) (g *dot.Graph) {
 	g = dot.NewGraph(dot.Directed)
+	edges := make(map[GenEdge[s.MicroprogramState]]int)
+	nodes := make(map[s.MicroprogramState]dot.Node)
+	for k, v_arr := range uprgm.Transitions {
+		for _, v := range v_arr {
+			edge_key := GenEdge[s.MicroprogramState]{k, v.StateDst}
+			if _, ok := edges[edge_key]; !ok {
+				edges[edge_key] = 0
+			}
+			edges[edge_key] += 1
+			if _, ok := nodes[edge_key.Src]; !ok {
+				nodes[edge_key.Src] = g.Node(
+					fmt.Sprintf("%#+v", edge_key.Src),
+				)
+			}
+			if _, ok := nodes[edge_key.Dst]; !ok {
+				nodes[edge_key.Dst] = g.Node(
+					fmt.Sprintf("%#+v", edge_key.Dst),
+				)
+			}
+		}
+	}
 	// TODO TODO TODO
 }
