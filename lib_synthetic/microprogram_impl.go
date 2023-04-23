e6dab3d49530cbedd7eb800914224fdc85e94a37
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-23 13:28:10 -0400

    actually generate acyclic quivers now

--- lib_synthetic/microprogram_impl.go
+++ lib_synthetic/microprogram_impl.go
@@ -48,7 +48,10 @@ func (gen *MicroprogramGenerator) RandomMicroprogram() (uprgm Microprogram) {
 		base_quiver.InsertEdge(top_node, rand.Intn(gen.P_n_states))
 	}
 	// Force it to be acyclic since cycles don't add anything to static constraint sets
-	base_quiver.CoerceAcyclic()
+	base_quiver.CoerceAcyclic(
+		map[int]struct{}{top_node: {}},
+		map[int]struct{}{failure_node: {}},
+	)
 	// Replace edges with random trees of random constraints
 	adj_list_map := base_quiver.ExtractAdjListAsMap(n_nodes)
 	uprgm_transitions := make(map[MicroprogramState][]MicroprogramTransition)
