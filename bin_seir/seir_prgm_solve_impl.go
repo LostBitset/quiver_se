966fb95be902777d07e88afaf4f90a525fac27be
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-25 10:03:32 -0400

    Going to make 2VA system now

--- bin_seir/seir_prgm_solve_impl.go
+++ bin_seir/seir_prgm_solve_impl.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	qse "github.com/LostBitset/quiver_se/lib"
 )
 
@@ -16,8 +18,15 @@ func (sp SeirPrgm) SolveForAssignmentsZ3(
 	if is_sat_ptr != nil && *&is_sat_ptr {
 		model_ptr := sctx.GetModel()
 		model := *model_ptr
+		fmt.Println("::: GOT MODEL :::")
+		fmt.Println(model)
+		fmt.Println("::: END MODEL :::")
 		assignments := ParseZ3ModelString(model)
 		assignments_ptr = &assignments
 	}
 	return
+}
+
+func ParseZ3ModelString(model string) (assignments []AssignedSMTValue) {
+	panic("TODO TODO TODO")
 }
