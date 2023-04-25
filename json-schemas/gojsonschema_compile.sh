bb83119e26f790e9714af29ade7c29d721a3fa46
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-24 20:52:32 -0400

    not actually what I wanted *sigh*

--- json-schemas/gojsonschema_compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MODULENAME="genseirschema"
-MODULEDIR="gen_seir_schema"
-
-gojsonschema -p $MODULENAME qse_dse_inp.schema.json > $MODULEDIR/generated_schema_inp.go
-gojsonschema -p $MODULENAME qse_dse_out.schema.json > $MODULEDIR/generated_schema_out.go
-
